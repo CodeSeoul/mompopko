aa3fa3e65adad68e5bdd35306a7b09646fa9acfb
Dasol-Jung <xenosophian@gmail.com>
2018-09-16 20:08:47 +0900

    firebase setup

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const firebase = require("firebase");
+const firebaseConfig = require("./config/keys");
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
