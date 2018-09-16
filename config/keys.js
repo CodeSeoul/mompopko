176e1e5d7b541fdfd6a5128ad7b3b5dc685aa00e
AidenJung <xenosophian@gmail.com>
2018-09-16 19:54:59 +0900

    delete firebase keys from git

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyCrvxG0KmEFbbfzH9Y-qh9CKHjKM_-0Rfk",
-  authDomain: "seoul-revisioned.firebaseapp.com",
-  databaseURL: "https://seoul-revisioned.firebaseio.com",
-  projectId: "seoul-revisioned",
-  storageBucket: "seoul-revisioned.appspot.com",
-  messagingSenderId: "852784944923"
-};
