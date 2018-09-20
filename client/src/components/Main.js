ee0450a2247c78e5fa6ad63f13227caa7b64d26b
Alex <rollinglex@gmail.com>
2018-09-21 03:44:28 +0900

    added routes

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,7 +1,19 @@
 import React from "react";
-
+import { Switch, Route } from "react-router-dom";
 const Main = () => {
-  return <div>Seoul Revisioned</div>;
+  return (
+    <main>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/people" component={People} />
+        <Route path="/person" component={Person} />
+        <Route path="/openings" component={Openings} />
+        <Route path="/opening" component={Opening} />
+        <Route path="/Data" component={Data} />
+        <Route path="/About" component={About} />
+      </Switch>
+    </main>
+  );
 };
 
 export default Main;
