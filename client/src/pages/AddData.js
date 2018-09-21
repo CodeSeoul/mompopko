8aee6ef2c7dad86c3f748b2d9cd500c6acafdb2d
Alex <rollinglex@gmail.com>
2018-09-22 06:36:55 +0900

    added psuedo code for AddData page

--- client/src/pages/AddData.js
+++ client/src/pages/AddData.js
@@ -1,9 +1,21 @@
 import React from "react";
 
+//features
+//should have icon to show drag and drop
+//user knows when file has been accepted
+//user knows when file has been processed
+//user aware of any errors
+//user can see summary of data to be added
+//user can decide to delete some parts of data before adding to firebase
+//user get notification of status of uploadin to firebase
+
 const AddData = () => {
   return (
     <div>
       <h1>AddData</h1>
+      {/* component to enter data */}
+      {/* table to see summary of data */}
+      {/* button to upload data to firebase */}
     </div>
   );
 };
