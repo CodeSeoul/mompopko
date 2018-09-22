f424945932fa2a5bec3e9bda9a41e7f0382af5f6
Dasol-Jung <xenosophian@gmail.com>
2018-09-22 10:52:36 +0900

    added toggle handler in hamburger component

--- client/src/components/container/Hamburger.js
+++ client/src/components/container/Hamburger.js
@@ -2,9 +2,20 @@ import React, { Component } from "react";
 import HamburgerStyle from "../../styles/presentational/HamburgerStyle";
 
 class Hamburger extends Component {
+  state = {
+    toggle: false
+  };
+
+  toggleHamburger = () => {
+    const newToggle = !this.state.toggle;
+    this.setState({
+      toggle: newToggle
+    });
+  };
+
   render() {
     return (
-      <HamburgerStyle>
+      <HamburgerStyle onClick={() => this.toggleHamburger}>
         <div className="hamburger-bar" />
         <div className="hamburger-bar" />
         <div className="hamburger-bar" />
