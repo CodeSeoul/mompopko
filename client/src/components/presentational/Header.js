b31e40207b0b099b24a2e90ce5123db036ba045a
Dasol-Jung <xenosophian@gmail.com>
2018-09-22 14:51:56 +0900

    created a search bar

--- client/src/components/presentational/Header.js
+++ client/src/components/presentational/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Brand from "./Brand";
 import Navbar from "./Navbar";
-
+import Search from "./Search";
 const Header = props => {
   return (
     <div className="header">
       <Brand />
       <Navbar />
+      <Search />
     </div>
   );
 };
---
+++ client/src/styles/presentational/SearchStyle.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import styleVar from "../variables";
+
+const SearchStyle = styled.div`
+  
+  }
+
+  /* Mobile */
+  @media only screen and (max-width: 740px) {
+  }
+`;
+
+export default SearchStyle;
