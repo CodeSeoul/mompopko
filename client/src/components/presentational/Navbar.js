49d61f4e698dc3120b9cca622a3d82665e7698ae
Dasol-Jung <xenosophian@gmail.com>
2018-09-22 10:55:05 +0900

    created HamburgerStyle styled-component

--- client/src/components/presentational/Navbar.js
+++ client/src/components/presentational/Navbar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavbarStyle from "../../styles/presentational/NavbarStyle";
 import { Link } from "react-router-dom";
+import Hamburger from "../container/Hamburger";
 
 const Navbar = props => {
   return (
     <NavbarStyle>
+      <Hamburger />
       <nav className="container">
         <ul>
           <Link to="/people">People</Link>
---
+++ client/src/styles/presentational/HamburgerStyle.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import styleVar from "../variables";
+
+const HamburgerStyle = styled.div``;
+
+export default HamburgerStyle;
