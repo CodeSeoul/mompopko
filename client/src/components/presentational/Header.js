0784b08e071f2c78d47a0509df452107960b63bf
Dasol-Jung <xenosophian@gmail.com>
2018-09-22 09:46:31 +0900

    created Header component

---
+++ client/src/components/presentational/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = props => {
+  return (
+    <div className="header">
+      <div className="brand">Seoul Re-visioned</div>
+      <div className="navbar" />
+    </div>
+  );
+};
+
+export default Header;
