d43639ef03d623cc91bb05399b5f5469c5d61632
Dasol-Jung <xenosophian@gmail.com>
2018-09-22 10:19:56 +0900

    changed brand font

--- client/src/styles/presentational/Brand.js
+++ client/src/styles/presentational/Brand.js
@@ -2,9 +2,11 @@ import styled from "styled-components";
 import styleVar from "../variables";
 
 const Brand = styled.div`
+  @import url("https://fonts.googleapis.com/css?family=Amatic+SC:400,700");
   border-bottom: 1px solid #ddd;
   text-align: center;
   font-size: 1.5rem;
+  font-family: Amatic SC;
   h1 {
     margin: 20px 0;
   }
