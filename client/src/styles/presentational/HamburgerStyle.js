cfb0e41f70bfc81b2bceb57f08f7159505f96ebc
Dasol-Jung <xenosophian@gmail.com>
2018-09-22 11:15:30 +0900

    added a responsive hamburger button

--- client/src/styles/presentational/HamburgerStyle.js
+++ client/src/styles/presentational/HamburgerStyle.js
@@ -1,6 +1,21 @@
 import styled from "styled-components";
 import styleVar from "../variables";
 
-const HamburgerStyle = styled.div``;
+const HamburgerStyle = styled.div`
+  display: none;
+  position: absolute;
+  right: 35px;
+  top: 48px;
+  cursor: pointer;
+  .hamburger-bar {
+    width: 28px;
+    height: 3px;
+    background-color: ${styleVar.secondaryDark};
+    margin-top: 5px;
+  }
+  @media only screen and (max-width: 1000px) {
+    display: block;
+  }
+`;
 
 export default HamburgerStyle;
