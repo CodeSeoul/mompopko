10bd6291420cc20c9cd651141edeff408dc29003
Dasol-Jung <xenosophian@gmail.com>
2018-09-22 14:57:51 +0900

    aligned search bar

--- client/src/styles/presentational/SearchStyle.js
+++ client/src/styles/presentational/SearchStyle.js
@@ -2,6 +2,15 @@ import styled from "styled-components";
 import styleVar from "../variables";
 
 const SearchStyle = styled.div`
+
+  display : flex;
+  align-items : center;
+  justify-content : center;
+  margin : 2rem;
+
+  button, form{
+    display : inline-block;
+  }
   
   }
 
