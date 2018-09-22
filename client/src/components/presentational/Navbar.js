b4864168a6cf238f2ceb246b92fa49ab59f2b4ec
Dasol-Jung <xenosophian@gmail.com>
2018-09-22 10:40:38 +0900

    Navbar basic structure

--- client/src/components/presentational/Navbar.js
+++ client/src/components/presentational/Navbar.js
@@ -3,7 +3,23 @@ import NavbarStyle from "../../styles/presentational/NavbarStyle";
 import { Link } from "react-router-dom";
 
 const Navbar = props => {
-  return <NavbarStyle>Navs</NavbarStyle>;
+  return (
+    <NavbarStyle>
+      <nav className="container">
+        <ul>
+          <Link to="/people">People</Link>
+          <Link to="/openings">New Openings</Link>
+          <Link to="/data">Data Trends</Link>
+          <Link to="/about">About</Link>
+        </ul>
+        <ul>
+          <Link to="#">FB</Link>
+          <Link to="#">IG</Link>
+          <Link to="#">YT</Link>
+        </ul>
+      </nav>
+    </NavbarStyle>
+  );
 };
 
 export default Navbar;
