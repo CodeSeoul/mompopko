79e131f353e5f6dc04034535dd8ae0c98b671945
Alex <rollinglex@gmail.com>
2018-09-23 03:52:48 +0900

    add drag events to addfile component

--- client/src/components/containers/AddFileContainer.js
+++ client/src/components/containers/AddFileContainer.js
@@ -1,10 +1,33 @@
 import React from "react";
 import AddFile from "../presentational/AddFile";
+import { log } from "util";
 class AddFileContainer extends React.Component {
   state = {};
 
+  _onDragEnter = () => {
+    console.log("Drag Enter");
+  };
+  _onDragLeave = () => {
+    console.log("Drag Leave");
+  };
+  _onDragOver = e => {
+    e.preventDefault();
+    console.log("Drag Over");
+  };
+  _onDrop = e => {
+    e.preventDefault();
+  };
+  componentDidMount() {}
   render() {
-    return <AddFile />;
+    return (
+      <AddFile
+        id="dragbox"
+        onDragOver={this._onDragOver}
+        onDrop={this._onDrop}
+        onDragEnter={this._onDragEnter}
+        onDragLeave={this._onDragLeave}
+      />
+    );
   }
 }
 export default AddFileContainer;
--- client/src/components/presentational/AddFile.js
+++ client/src/components/presentational/AddFile.js
@@ -1,9 +1,21 @@
 import React from "react";
 import AddFile from "../../styles/components/AddFile/index";
-const Addfile = () => {
+const Addfile = props => {
   const { Wrapper } = AddFile;
+  const { onDragOver, onDrop, onDragLeave, onDragEnter } = props;
   console.log("AddData", Wrapper);
-  return <Wrapper>AddFiles</Wrapper>;
+  return (
+    <Wrapper
+      onDragOver={e => onDragOver(e)}
+      onDragEnter={e => onDragEnter(e)}
+      onDragLeave={e => onDragLeave(e)}
+      onDrop={e => {
+        onDrop(e, "wip");
+      }}
+    >
+      AddFiles
+    </Wrapper>
+  );
 };
 
 export default Addfile;
