78a0d20617cbb80056213848bc6d6a9aeb5e7c8f
Dasol-Jung <xenosophian@gmail.com>
2018-09-22 11:59:16 +0900

    added css grid and flex to Navbar

--- client/src/styles/presentational/NavbarStyle.js
+++ client/src/styles/presentational/NavbarStyle.js
@@ -1,6 +1,23 @@
 import styled from "styled-components";
 import styleVar from "../variables";
 
-const NavbarStyle = styled.div``;
+const NavbarStyle = styled.div`
+  nav {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    ul {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-around;
+      a {
+        padding: 0 20px;
+        text-decoration: none;
+        color: ${styleVar.secondaryDark};
+        display: block;
+      }
+    }
+  }
+`;
 
 export default NavbarStyle;
