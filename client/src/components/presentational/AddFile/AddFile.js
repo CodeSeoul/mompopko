1c87cc60542c6fe111f70644ff00cd727ce36afd
Alex <rollinglex@gmail.com>
2018-09-24 03:37:24 +0900

    moved AddFile.js to newly created AddFile dir in presentational dir

--- client/src/components/presentational/AddFile/AddFile.js
+++ client/src/components/presentational/AddFile/AddFile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import AddFile from "../../styles/components/AddFile/index";
+import AddFile from "../../../styles/components/AddFile/index";
 const Addfile = props => {
   const { Wrapper } = AddFile;
   const { onDragOver, onDrop, onDragLeave, onDragEnter, className } = props;
