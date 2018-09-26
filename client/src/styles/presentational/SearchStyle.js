dc1f49cad31c26208a5bec2608b8a85ae508f009
Dasol-Jung <xenosophian@gmail.com>
2018-09-26 15:42:01 +0900

    changed search bar style

--- client/src/styles/presentational/SearchStyle.js
+++ client/src/styles/presentational/SearchStyle.js
@@ -6,11 +6,30 @@ const SearchStyle = styled.div`
   display : flex;
   align-items : center;
   justify-content : center;
-  margin : 2rem;
+  box-sizing : border-box;
+  margin : 20px 0;
 
-  button, form{
+  button {
     display : inline-block;
+    border-radius : 5% 0% 0% 5%;
+    height : 20px;
+    border : 0.5px ${styleVar.primaryDark} solid;
+    background-color : ${styleVar.secondaryDark};
+    i{
+      color : ${styleVar.primaryLight};
+    }
   }
+
+  form{
+    display : inline-block;
+
+    input{
+      border : 0.5px ${styleVar.primaryDark} solid;
+      border-radius : 0% 5% 5% 0%;
+      height : 16px;
+    }
+  }
+
   
   }
 
