fb2d8dccdcf0aaa15ae4a6d33bbaffd7a565f9a4
Dasol-Jung <xenosophian@gmail.com>
2018-09-26 18:36:07 +0900

    fetch people test  data from firebase

--- client/src/components/containers/PeopleContainer.js/PeopleContainer.js
+++ client/src/components/containers/PeopleContainer.js/PeopleContainer.js
@@ -1,7 +1,30 @@
 import React, { Component } from "react";
+import config from "../../../config/config";
+const firebase = require("firebase");
+require("firebase/firestore");
+
+firebase.initializeApp(config);
+const db = firebase.firestore();
+
+db.settings({
+  timestampsInSnapshots: true
+});
 
 class PeopleContainer extends Component {
+  state = {
+    people: []
+  };
+  componentDidMount() {
+    db.collection("people")
+      .get()
+      .then(collection => {
+        const people = collection.docs.map(doc => doc.data());
+        this.setState({ people: people });
+      });
+  }
+
   render() {
+    console.log(this.state);
     return <div />;
   }
 }
