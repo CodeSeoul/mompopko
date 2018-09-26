f56e46b43d522197dbc59d1de39c5ded5d626b7d
Alex <rollinglex@gmail.com>
2018-09-27 03:25:05 +0900

    display file data to table

--- client/src/components/presentational/AddFile/FileData.js
+++ client/src/components/presentational/AddFile/FileData.js
@@ -4,9 +4,24 @@ const FileData = props => {
   const { data } = props;
   let render =
     data.length > 0 ? (
-      <div>
+      <section>
         <p>Number of businesses: {data.length - 1}</p>
-      </div>
+        <table>
+          <tbody>
+            {data.map((business, i) => {
+              return (
+                <tr key={i}>
+                  <th>{business[2]}</th>
+                  <th>{business[3] + " " + business[8]}</th>
+                  <th>{business[6]}</th>
+                  <th>{business[7]}</th>
+                  <th>{business[11]}</th>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </section>
     ) : null;
 
   return render;
