14fc509f9f850081c865828eb547c91fb4746282
Dasol-Jung <xenosophian@gmail.com>
2018-09-27 17:34:56 +0900

    created AddPeople page

---
+++ client/src/pages/AddPeople.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class AddPeople extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header>
+          <h1>Add People</h1>
+        </Header>
+        <AddPeopleContainer />
+      </div>
+    );
+  }
+}
+export default AddPeople;
