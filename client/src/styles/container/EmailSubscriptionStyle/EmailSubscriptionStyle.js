3487c74af5f1f998846c8efe9251b0300eaaf7ad
Dasol-Jung <xenosophian@gmail.com>
2018-10-03 18:26:41 +0900

    making EmailSubscription component edge round

--- client/src/styles/container/EmailSubscriptionStyle/EmailSubscriptionStyle.js
+++ client/src/styles/container/EmailSubscriptionStyle/EmailSubscriptionStyle.js
@@ -6,15 +6,20 @@ const EmailSubscriptionStyle = styled.div`
     display: flex;
     flex-direction: row;
   }
+
   .icon-wrapper {
     height: 24px;
     padding: 0 4px;
     background-color: ${styleVar.secondaryDark};
     color: ${styleVar.primaryLight};
+    border-radius: 5px 0 0 5px;
   }
+
   form {
     height: 20px;
+
     input {
+      border-radius: 0 5px 5px 0;
       height: 20px;
       padding: 0;
       height: inherit;
