cb0db7518a49f1d764d3a15fe809c141a6fd9381
Dasol-Jung <xenosophian@gmail.com>
2018-10-03 15:02:21 +0900

    adjusting each person's image size

--- client/src/styles/components/Person/PersonStyle.js
+++ client/src/styles/components/Person/PersonStyle.js
@@ -4,11 +4,15 @@ import styledVar from "../../variables";
 const Person = styled.div`
   .person-card {
     overflow: hidden;
+    width: 100%;
+    height: 400px;
   }
 
   img {
-    width: 100%;
-    height: auto;
+    object-fit: cover;
+    width: inherit;
+    object-position: 50% top;
+    height: 100%;
   }
 
   .description {
