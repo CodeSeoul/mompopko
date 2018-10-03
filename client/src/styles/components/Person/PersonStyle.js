e86afc02a4328c9df8e3960903727e83c5d41273
Dasol-Jung <xenosophian@gmail.com>
2018-10-03 14:25:56 +0900

    adding transition effect on hover to Person component

--- client/src/styles/components/Person/PersonStyle.js
+++ client/src/styles/components/Person/PersonStyle.js
@@ -4,6 +4,7 @@ import styledVar from "../../variables";
 const Person = styled.div`
   .person-card {
     width: 100%;
+    overflow: hidden;
   }
 
   img {
@@ -11,6 +12,16 @@ const Person = styled.div`
     max-width: 100%;
   }
   .description {
+    color: ${styledVar.primaryDark};
+    background-color: ${styledVar.primaryLight};
+    opacity: 0.8;
+    transform: translateY(-40px);
+    height: 40px;
+    transition: all 0.5s;
+    :hover {
+      height: 80px;
+      transform: translateY(-80px);
+    }
   }
 `;
 
