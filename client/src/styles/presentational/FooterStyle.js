d5d335e82e1e8ea0386067123ca63f741647ccf7
Dasol-Jung <xenosophian@gmail.com>
2018-10-03 16:45:49 +0900

    changing footer color and adding css grid to it

--- client/src/styles/presentational/FooterStyle.js
+++ client/src/styles/presentational/FooterStyle.js
@@ -1,6 +1,13 @@
 import styled from "styled-components";
 import styleVar from "../variables";
 
-const FooterStyle = styled.div``;
+const FooterStyle = styled.div`
+  height: 200px;
+  color: ${styleVar.primaryLight};
+  background-color: ${styleVar.primaryDark};
+  display: grid;
+  align-items: center;
+  justify-items: center;
+`;
 
 export default FooterStyle;
