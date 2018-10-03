60dc15e8d753ea464771ae4773f876dbf4fbde95
Dasol-Jung <xenosophian@gmail.com>
2018-10-03 11:35:15 +0900

    applied css grid to people page

--- client/src/styles/components/People/People.js
+++ client/src/styles/components/People/People.js
@@ -1,5 +1,17 @@
 import styled from "styled-components";
 
-const People = styled.div``;
+const People = styled.div`
+  padding: 3rem 5rem;
+
+  .container {
+    margin-top: 30px;
+  }
+
+  .grid-container {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 1rem;
+  }
+`;
 
 export default People;
--- client/src/styles/components/Person/PersonStyle.js
+++ client/src/styles/components/Person/PersonStyle.js
@@ -1,5 +1,17 @@
 import styled from "styled-components";
+import styledVar from "../../variables";
 
-const Person = styled.div``;
+const Person = styled.div`
+  .person-card {
+    width: 100%;
+  }
+
+  img {
+    height: auto;
+    max-width: 100%;
+  }
+  .description {
+  }
+`;
 
 export default Person;
