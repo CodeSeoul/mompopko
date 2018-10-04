b1e28bb940206a2ac90a522e2e9c9096700bd941
Dasol-Jung <xenosophian@gmail.com>
2018-10-04 21:57:08 +0900

    changed PeopleInfo component into container one

--- client/src/components/presentational/PeopleInfo/PeopleInfo.js
+++ client/src/components/presentational/PeopleInfo/PeopleInfo.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const PeopleInfo = props => {
-  console.log(props);
-  return (
-    <div>
-      <div>Hello</div>
-    </div>
-  );
-};
+class PeopleInfo extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Hello</div>
+      </div>
+    );
+  }
+}
 
 export default PeopleInfo;
