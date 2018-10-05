fd048c45282e61a54f26cfbb5f75d7da7067b657
Dasol-Jung <xenosophian@gmail.com>
2018-10-06 01:31:54 +0900

    added left, right button and state

--- client/src/components/containers/Carousel/Carousel.js
+++ client/src/components/containers/Carousel/Carousel.js
@@ -2,10 +2,16 @@ import React from "react";
 import CarouselStyle from "../../../styles/container/CarouselStyle/CarouselStyle";
 
 class Carousel extends React.Component {
+  state = {
+    slideIndex: 0
+  };
+
   render() {
     return (
       <CarouselStyle>
+        <div className="left-button" />
         <div className="wrapper">{this.props.children}</div>;
+        <div className="right-button" />
       </CarouselStyle>
     );
   }
