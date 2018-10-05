3f33e5221c4bdc44f194ac8fdf5d1ec9ffd24715
Dasol-Jung <xenosophian@gmail.com>
2018-10-06 01:32:58 +0900

    styling carousel wrapper

--- client/src/styles/container/CarouselStyle/CarouselStyle.js
+++ client/src/styles/container/CarouselStyle/CarouselStyle.js
@@ -1,5 +1,16 @@
 import styled from "styled-components";
 
-const CarouselStyle = styled.div``;
+const CarouselStyle = styled.div`
+  .wrapper {
+    overflow: hidden;
+    width: 300px;
+    height: 400px;
+    img {
+      width: inherit;
+      height: inherit;
+      object-fit: cover;
+    }
+  }
+`;
 
 export default CarouselStyle;
