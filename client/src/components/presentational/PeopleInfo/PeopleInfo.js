7465a960debbe28ec065c9d6fa25e4636f4bf0f4
Dasol-Jung <xenosophian@gmail.com>
2018-10-05 22:22:39 +0900

    pass person data to PeopleInfo component

--- client/src/components/presentational/PeopleInfo/PeopleInfo.js
+++ client/src/components/presentational/PeopleInfo/PeopleInfo.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Person from "../Person/Person";
 
 class PeopleInfo extends React.Component {
   render() {
+    const person = this.props.person;
+
     return (
       <div>
         <div>Hello</div>
