76ce9d695e075aef9158acfaf5560d66f2650bc9
Dasol Jung <xenosophian@gmail.com>
2018-10-09 13:51:42 +0900

    created EpitPeopleContainer

---
+++ client/src/components/containers/Admin/EditPeopleContainer/EditPeopleContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class EditPeopleContainer extends Component {
+  render() {
+    return <div>EditPeople</div>;
+  }
+}
+
+export default EditPeopleContainer;
--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
+import EditPeopleContainer from "../components/containers/Admin/EditPeopleContainer/EditPeopleContainer";
+
 const Admin = () => {
   return (
     <Switch>
-      <Route path="/admin/people" />
+      <Route path="/admin/people" component={EditPeopleContainer} />
       <Route path="/admin/openings" />
       <Route path="/admin/data" />
       <Route path="/admin/about" />
