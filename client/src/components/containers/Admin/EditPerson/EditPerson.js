3adb9ed5c9a973eb48cef4128e2842bbddc6a3f9
Dasol Jung <xenosophian@gmail.com>
2018-10-09 17:03:22 +0900

    styling EditPeople component

--- client/src/components/containers/Admin/EditPerson/EditPerson.js
+++ client/src/components/containers/Admin/EditPerson/EditPerson.js
@@ -57,98 +57,102 @@ class EditPerson extends Component {
     return (
       <React.Fragment>
         <div>Editing this</div>
-        <form onSubmit={e => this.uploadHandler(e)} action="POST">
-          <span>name</span>
-          <input
-            value={name}
-            onChange={e => this.changeHandler(e)}
-            name="name"
-            type="text"
-          />
-          <span>occupation</span>
-          <input
-            value={occupation}
-            onChange={e => this.changeHandler(e)}
-            name="occupation"
-            type="text"
-          />
-          <span>business</span>
-          <input
-            value={business}
-            onChange={e => this.changeHandler(e)}
-            name="business"
-            type="text"
-          />
-          <span>location</span>
-          <input
-            value={location}
-            onChange={e => this.changeHandler(e)}
-            name="location"
-            type="text"
-          />
-          <span>category</span>
-          <input
-            value={category}
-            onChange={e => this.changeHandler(e)}
-            name="category"
-            type="text"
-          />
-          <span>started</span>
-          <input
-            value={started}
-            onChange={e => this.changeHandler(e)}
-            name="started"
-            type="date"
-          />
-          <span>Website URL</span>
-          <input
-            value={urls.website}
-            group="url"
-            onChange={e => this.changeHandler(e)}
-            name="website"
-            type="url"
-          />
-          <span>Facebook URL</span>
-          <input
-            value={urls.facebook}
-            group="url"
-            onChange={e => this.changeHandler(e)}
-            name="facebook"
-            type="url"
-          />
-          <span>Instagram URL</span>
-          <input
-            value={urls.instagram}
-            group="url"
-            onChange={e => this.changeHandler(e)}
-            name="instagram"
-            type="url"
-          />
-          <span>Youtube URL</span>
-          <input
-            value={urls.youtube}
-            group="url"
-            onChange={e => this.changeHandler(e)}
-            name="youtube"
-            type="url"
-          />
-          <span>address</span>
-          <input
-            value={address}
-            onChange={e => this.changeHandler(e)}
-            name="address"
-            type="text"
-          />
-          <span>interview</span>
-          <textarea
-            value={interview}
-            onChange={e => this.changeHandler(e)}
-            name="interview"
-            cols="30"
-            rows="10"
-          />
-          <button>submit</button>
-        </form>
+        <div className="profile-box-container">
+          <div className="profile-box">
+            <form onSubmit={e => this.uploadHandler(e)} action="POST">
+              <h5>name</h5>
+              <input
+                value={name}
+                onChange={e => this.changeHandler(e)}
+                name="name"
+                type="text"
+              />
+              <h5>occupation</h5>
+              <input
+                value={occupation}
+                onChange={e => this.changeHandler(e)}
+                name="occupation"
+                type="text"
+              />
+              <h5>business</h5>
+              <input
+                value={business}
+                onChange={e => this.changeHandler(e)}
+                name="business"
+                type="text"
+              />
+              <h5>location</h5>
+              <input
+                value={location}
+                onChange={e => this.changeHandler(e)}
+                name="location"
+                type="text"
+              />
+              <h5>category</h5>
+              <input
+                value={category}
+                onChange={e => this.changeHandler(e)}
+                name="category"
+                type="text"
+              />
+              <h5>started</h5>
+              <input
+                value={started}
+                onChange={e => this.changeHandler(e)}
+                name="started"
+                type="date"
+              />
+              <h5>Website URL</h5>
+              <input
+                value={urls.website}
+                group="url"
+                onChange={e => this.changeHandler(e)}
+                name="website"
+                type="url"
+              />
+              <h5>Facebook URL</h5>
+              <input
+                value={urls.facebook}
+                group="url"
+                onChange={e => this.changeHandler(e)}
+                name="facebook"
+                type="url"
+              />
+              <h5>Instagram URL</h5>
+              <input
+                value={urls.instagram}
+                group="url"
+                onChange={e => this.changeHandler(e)}
+                name="instagram"
+                type="url"
+              />
+              <h5>Youtube URL</h5>
+              <input
+                value={urls.youtube}
+                group="url"
+                onChange={e => this.changeHandler(e)}
+                name="youtube"
+                type="url"
+              />
+              <h5>address</h5>
+              <input
+                value={address}
+                onChange={e => this.changeHandler(e)}
+                name="address"
+                type="text"
+              />
+              <h5>interview</h5>
+              <textarea
+                value={interview}
+                onChange={e => this.changeHandler(e)}
+                name="interview"
+                cols="30"
+                rows="10"
+              />
+              <button>submit</button>
+            </form>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
--- client/src/styles/components/containers/admin/EditPeopleStyle/EditPeopleStyle.js
+++ client/src/styles/components/containers/admin/EditPeopleStyle/EditPeopleStyle.js
@@ -1,5 +1,42 @@
 import styled from "styled-components";
+import styleVar from "../../../../variables";
 
-const EditPeopleStyle = styled.div``;
+const EditPeopleStyle = styled.div`
+  .profile-box-container {
+    padding: 2rem;
+    background-color: ${styleVar.secondaryLight};
+
+    h5 {
+      font-family: "Caveat";
+      text-transform: capitalize;
+      font-size: 1.5rem;
+      margin: 0.7rem 0;
+    }
+
+    input,
+    textarea {
+      width: 80%;
+      font-size: 1.3rem;
+      margin-left: 3rem;
+      display: inline-block;
+      border: none;
+      background-color: ${styleVar.secondaryLight};
+      :focus {
+        background-color: ${styleVar.primaryLight};
+        border: 1px solid rgb(50, 50, 213);
+      }
+    }
+
+    button {
+      display: block;
+      margin: 1rem auto;
+    }
+  }
+
+  @media only screen and (max-width: 900px) {
+    grid-template-columns: 1fr;
+    padding: 1rem 3rem;
+  }
+`;
 
 export default EditPeopleStyle;
