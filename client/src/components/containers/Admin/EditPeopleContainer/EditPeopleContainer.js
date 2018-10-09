28a3b5aaac83cb7d57cd91b6887e8f9e6d59740b
Dasol Jung <xenosophian@gmail.com>
2018-10-09 14:01:01 +0900

    fetch people data from firebase

--- client/src/components/containers/Admin/EditPeopleContainer/EditPeopleContainer.js
+++ client/src/components/containers/Admin/EditPeopleContainer/EditPeopleContainer.js
@@ -1,7 +1,25 @@
 import React, { Component } from "react";
+import FbApp from "../../../../config/firebase";
 import EditPeopleStyle from "../../../../styles/components/containers/admin/EditPeopleStyle/EditPeopleStyle";
 
+const db = FbApp.firestore();
+
+db.settings({
+  timestampsInSnapshots: true
+});
+
 class EditPeopleContainer extends Component {
+  componentDidMount() {
+    db.collection("people")
+      .get()
+      .then(collection => {
+        const people = collection.docs.map(doc => {
+          console.log(doc.data(), "doc");
+          return { ...doc.data(), id: doc.id };
+        });
+        this.setState({ people: people, isLoaded: true });
+      });
+  }
   render() {
     return (
       <EditPeopleStyle>
