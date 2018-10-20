047432cd46f4cb60aa2bb2ab62522f4355337969
Dasol-Jung <xenosophian@gmail.com>
2018-10-20 10:01:49 +0900

    styling interview section

--- client/src/components/presentational/PeopleInfo/PeopleInfo.js
+++ client/src/components/presentational/PeopleInfo/PeopleInfo.js
@@ -34,73 +34,75 @@ class PeopleInfo extends React.Component {
 
     return (
       <PeopleInfoStyle>
-        <Carousel>{images}</Carousel>
-        <div className="profile-box-container">
-          <ul className="profile-box">
-            <li>
-              <h5>Occupation</h5>
-              <span>{person.occupation}</span>
-            </li>
-            <li>
-              <h5>Affiliated Business</h5>
-              <span>{person.business}</span>
-            </li>
-            <li>
-              <h5>Location</h5>
-              <span>{person.location}</span>
-            </li>
-            <li>
-              <h5>Category</h5>
-              <span>{person.category}</span>
-            </li>
-            <li>
-              <h5>Started</h5>
-              <span>{person.started}</span>
-            </li>
-            <li>
-              <h5>URL</h5>
-              <ul>
-                <a href={person.urls.website}>
-                  <i className="fas fa-home" />
-                </a>
-                <a href={person.urls.facebook}>
-                  <i className="fab fa-facebook" />
-                </a>
-                <a href={person.urls.instagram}>
-                  <i className="fab fa-instagram" />
-                </a>
-                <a href={person.urls.youtube}>
-                  <i className="fab fa-youtube" />
-                </a>
-              </ul>
-            </li>
-            <li>
-              <h5>Address</h5>
-              <span>{person.address}</span>
-              {!this.state.geoLocation ? null : (
-                <GoogleMap
-                  id="map"
-                  option={{
-                    center: {
-                      lat: this.state.geoLocation.lat,
-                      lng: this.state.geoLocation.lng
-                    },
-                    zoom: 17
-                  }}
-                  onMapLoad={map => {
-                    const market = new window.google.maps.Marker({
-                      position: {
+        <div className="grid-container">
+          <Carousel>{images}</Carousel>
+          <div className="profile-box-container">
+            <ul className="profile-box">
+              <li>
+                <h5>Occupation</h5>
+                <span>{person.occupation}</span>
+              </li>
+              <li>
+                <h5>Affiliated Business</h5>
+                <span>{person.business}</span>
+              </li>
+              <li>
+                <h5>Location</h5>
+                <span>{person.location}</span>
+              </li>
+              <li>
+                <h5>Category</h5>
+                <span>{person.category}</span>
+              </li>
+              <li>
+                <h5>Started</h5>
+                <span>{person.started}</span>
+              </li>
+              <li>
+                <h5>URL</h5>
+                <ul>
+                  <a href={person.urls.website}>
+                    <i className="fas fa-home" />
+                  </a>
+                  <a href={person.urls.facebook}>
+                    <i className="fab fa-facebook" />
+                  </a>
+                  <a href={person.urls.instagram}>
+                    <i className="fab fa-instagram" />
+                  </a>
+                  <a href={person.urls.youtube}>
+                    <i className="fab fa-youtube" />
+                  </a>
+                </ul>
+              </li>
+              <li>
+                <h5>Address</h5>
+                <span>{person.address}</span>
+                {!this.state.geoLocation ? null : (
+                  <GoogleMap
+                    id="map"
+                    option={{
+                      center: {
                         lat: this.state.geoLocation.lat,
                         lng: this.state.geoLocation.lng
                       },
-                      map: map,
-                      title: "business"
-                    });
-                  }}
-                />
-              )}
-            </li>
-          </ul>
+                      zoom: 17
+                    }}
+                    onMapLoad={map => {
+                      const market = new window.google.maps.Marker({
+                        position: {
+                          lat: this.state.geoLocation.lat,
+                          lng: this.state.geoLocation.lng
+                        },
+                        map: map,
+                        title: "business"
+                      });
+                    }}
+                  />
+                )}
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="interview">{person.interview}</div>
       </PeopleInfoStyle>
--- client/src/styles/presentational/PeopleInfoStyle.js
+++ client/src/styles/presentational/PeopleInfoStyle.js
@@ -2,51 +2,58 @@ import styled from "styled-components";
 import styleVar from "../variables";
 
 const PeopleInfoStyle = styled.div`
-  padding: 1rem 10%;
-  display: grid;
-  grid-template-columns: 30% 70%;
-  grid-gap: 40px;
-  align-items: start;
+  .grid-container {
+    padding: 1rem 10%;
+    display: grid;
+    grid-template-columns: 30% 70%;
+    grid-gap: 40px;
+    align-items: start;
 
-  #map {
-    width: 100%;
-    height: 200px;
-    margin-top: 2rem;
-  }
+    #map {
+      width: 100%;
+      height: 200px;
+      margin-top: 2rem;
+    }
 
-  .profile-box-container {
-    background-color: ${styleVar.secondaryLight};
+    .profile-box-container {
+      background-color: ${styleVar.secondaryLight};
 
-    ul {
-      list-style: none;
-      margin-left: 3rem;
-      padding: 0;
+      ul {
+        list-style: none;
+        margin-left: 3rem;
+        padding: 0;
 
-      a {
-        margin-right: 2rem;
-        font-size: 1.3rem;
-        color: ${styleVar.secondaryDark};
-        :hover {
-          color: ${styleVar.primaryDark};
+        a {
+          margin-right: 2rem;
+          font-size: 1.3rem;
+          color: ${styleVar.secondaryDark};
+          :hover {
+            color: ${styleVar.primaryDark};
+          }
         }
       }
-    }
 
-    h5 {
-      font-family: "Caveat";
-      font-size: 1.5rem;
-      margin: 0.7rem 0;
-    }
+      h5 {
+        font-family: "Caveat";
+        font-size: 1.5rem;
+        margin: 0.7rem 0;
+      }
 
-    span {
-      margin-left: 3rem;
-      display: inline-block;
+      span {
+        margin-left: 3rem;
+        display: inline-block;
+      }
     }
   }
-
+  .interview {
+    overflow-wrap: break-word;
+    padding: 1rem 10%;
+  }
   @media only screen and (max-width: 900px) {
-    grid-template-columns: 1fr;
-    padding: 1rem 3rem;
+    .grid-container {
+      grid-template-columns: 1fr;
+      padding: 1rem 3rem;
+    }
   }
 `;
 
