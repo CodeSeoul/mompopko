da0b42ba5f07f9249265e6e41aaf3a8f5f77cd8b
Dasol-Jung <xenosophian@gmail.com>
2018-10-26 21:43:32 +0900

    added a form to AddOpenings component

--- client/src/components/containers/Admin/EditOpeningsContainer/AddOpeningsContainer/AddOpeningsContainer.js
+++ client/src/components/containers/Admin/EditOpeningsContainer/AddOpeningsContainer/AddOpeningsContainer.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import AddOpenings from "../../../../presentational/Admin/Openings/AddOpenings/AddOpenings";
 
 class AddOpeningsContainer extends Component {
+  state = {};
+
   render() {
     return <AddOpenings>Add Openings</AddOpenings>;
   }
--- client/src/components/presentational/Admin/Openings/AddOpenings/AddOpenings.js
+++ client/src/components/presentational/Admin/Openings/AddOpenings/AddOpenings.js
@@ -1,7 +1,97 @@
 import React from "react";
 
 const AddOpenings = props => {
-  return <div>Add Openings</div>;
+  return (
+    <div>
+      <form>
+        <h5>Business Catagory</h5>
+        <input
+          spellCheck="false"
+          //value={category}
+          name="category"
+          type="text"
+        />
+        <h5>Registration Date</h5>
+        <input
+          spellCheck="false"
+          //value={registration}
+          name="registration"
+          type="date"
+        />
+        <h5>Business Name</h5>
+        <input
+          spellCheck="false"
+          //value={name}
+          name="name"
+          type="text"
+        />
+        <h5>Location(new system)</h5>
+        <input
+          spellCheck="false"
+          //value={newLocation}
+          name="newLocation"
+          type="text"
+        />
+        <h5>Location(old system)</h5>
+        <input
+          spellCheck="false"
+          //value={oldLocation}
+          name="oldLocation"
+          type="text"
+        />
+        <h5>Area</h5>
+        <input
+          spellCheck="false"
+          //value={area}
+          name="area"
+          type="text"
+        />
+        <h5>Telephone</h5>
+        <input
+          spellCheck="false"
+          //value={telephone}
+          name="telephone"
+          type="text"
+        />
+        <h5>Opening Date</h5>
+        <input
+          spellCheck="false"
+          //value={opening}
+          name="opening"
+          type="date"
+        />
+        <h5>Zipcode(new system)</h5>
+        <input
+          spellCheck="false"
+          //value={newZipcode}
+          name="newZipcode"
+          type="text"
+        />
+        <h5>Zipcode(old system)</h5>
+        <input
+          spellCheck="false"
+          //value={oldZipcode}
+          name="oldZipcode"
+          type="text"
+        />
+        <h5>Construction date</h5>
+        <input
+          spellCheck="false"
+          //value={construction}
+          name="construction"
+          type="date"
+        />
+        <h5>Type</h5>
+        <input
+          spellCheck="false"
+          //value={type}
+          name="type"
+          type="text"
+        />
+        <button>submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default AddOpenings;
