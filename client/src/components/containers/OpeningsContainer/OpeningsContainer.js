32100738ec12887e80810abf381571b9ed7c1fe2
Dasol-Jung <xenosophian@gmail.com>
2018-10-26 21:00:55 +0900

    fetching data from firebase cloud storage

--- client/src/components/containers/OpeningsContainer/OpeningsContainer.js
+++ client/src/components/containers/OpeningsContainer/OpeningsContainer.js
@@ -1,9 +1,29 @@
 import React, { Component } from "react";
 import OpeningsStyle from "../../../styles/components/containers/OpeningsStyle/OpeningsStyle";
 import Opening from "../../presentational/Opening/Opening";
+import FbApp from "../../../config/firebase";
+
+const db = FbApp.firestore();
+
+db.settings({
+  timestampsInSnapshots: true
+});
 
 class OpeningsContainer extends Component {
   state = {};
+
+  componentDidMount() {
+    db.collection("openings")
+      .get()
+      .then(collection => {
+        const openings = collection.docs.map(doc => {
+          return { ...doc.data(), id: doc.id };
+        });
+        this.setState({
+          openings: openings
+        });
+      });
+  }
 
   render() {
     return (
