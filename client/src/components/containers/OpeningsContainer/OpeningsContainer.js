43a8cf96ec2ebdb85b2067d79464839053cc00e0
Dasol-Jung <xenosophian@gmail.com>
2018-10-26 20:44:36 +0900

    created Opening component

--- client/src/components/containers/OpeningsContainer/OpeningsContainer.js
+++ client/src/components/containers/OpeningsContainer/OpeningsContainer.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import OpeningsStyle from "../../../styles/components/containers/OpeningsStyle/OpeningsStyle";
+import Opening from "../../presentational/Opening/Opening";
 
 class OpeningsContainer extends Component {
   state = {};
 
   render() {
-    return <OpeningsStyle>Hello</OpeningsStyle>;
+    return (
+      <OpeningsStyle>
+        <Opening />
+      </OpeningsStyle>
+    );
   }
 }
 
---
+++ client/src/components/presentational/Opening/Opening.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Opening = props => {
+  return <div>Opening</div>;
+};
+
+export default Opening;
