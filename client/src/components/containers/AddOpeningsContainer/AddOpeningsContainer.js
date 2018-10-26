6acab0710eaddd3efc17fb7fe474641292def4a0
Dasol-Jung <xenosophian@gmail.com>
2018-10-26 21:03:45 +0900

    added AddOpeingsContainer component

---
+++ client/src/components/containers/AddOpeningsContainer/AddOpeningsContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class AddOpeningsContainer extends Component {
+  render() {
+    return <div>Add Openings</div>;
+  }
+}
+
+export default AddOpeningsContainer;
