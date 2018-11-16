2b881758d6267abb7e5a0ebad1a96dbd11d5c96a
Dasol-Jung <xenosophian@gmail.com>
2018-11-16 21:22:39 +0900

    adding signInHandler

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,18 +1,40 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-
+import FbApp from "../config/firebase";
 import EditPeopleContainer from "../components/containers/Admin/EditPeopleContainer/EditPeopleContainer";
 import EditOpeningsContainer from "../components/containers/Admin/EditOpeningsContainer/EditOpeningsContainer";
 import Login from "../components/containers/Login/Login";
 
+const auth = FbApp.auth();
+
 const Admin = () => {
-  let submitHandler = (email, password) => {
-    console.log(email, password);
+  let signInHandler = (email, password) => {
+    if (auth.currentUser) {
+      auth.signOut();
+    }
+    auth.signInWithEmailAndPassword(email, password).catch(error => {
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      if (errorCode === "auth/wrong-password") {
+        alert("Wrong Password");
+      } else {
+        alert(errorMessage);
+      }
+    });
   };
+
+  auth.onAuthStateChanged(user => {
+    if (user) {
+      const { uid } = user;
+      console.log(user);
+    } else {
+    }
+  });
+
   return (
     <React.Fragment>
       <Login
-        submitHandler={(email, password) => submitHandler(email, password)}
+        submitHandler={(email, password) => signInHandler(email, password)}
       />
       <Switch>
         <Route path="/admin/people" component={EditPeopleContainer} />
