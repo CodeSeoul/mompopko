5deb650b5a04adaf4963f83baa38a28f326bf270
Dasol-Jung <xenosophian@gmail.com>
2018-11-16 19:57:43 +0900

    adding changeHandler to Login component

--- client/src/components/containers/Login/Login.js
+++ client/src/components/containers/Login/Login.js
@@ -3,20 +3,43 @@ import PropTypes from "prop-types";
 
 class Login extends Component {
   state = {
-    id: "",
+    email: "",
     password: ""
+  };
+
+  changeHandler = e => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    this.setState({
+      [name]: value
+    });
   };
 
   render() {
     <form>
-      <input type="email" />
-      <input type="password" />
+      <input
+        value={this.state.email}
+        name="email"
+        onChange={e => {
+          this.changeHandler(e);
+        }}
+        type="email"
+      />
+      <input
+        name="password"
+        value={this.state.password}
+        onChange={e => {
+          this.changeHandler(e);
+        }}
+        type="password"
+      />
     </form>;
   }
 }
 
 Login.propTypes = {
-  id: PropTypes.any.isRequired,
+  email: PropTypes.any.isRequired,
   password: PropTypes.any.isRequired
 };
 export default Login;
