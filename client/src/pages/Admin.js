7963268f7186cf0bf4a8294f3458b1f050ae4a1c
Dasol-Jung <xenosophian@gmail.com>
2018-11-16 20:00:51 +0900

    importing Login component to Admin one

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -2,17 +2,20 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import EditPeopleContainer from "../components/containers/Admin/EditPeopleContainer/EditPeopleContainer";
-
 import EditOpeningsContainer from "../components/containers/Admin/EditOpeningsContainer/EditOpeningsContainer";
+import Login from "../components/containers/Login/Login";
 
 const Admin = () => {
   return (
-    <Switch>
-      <Route path="/admin/people" component={EditPeopleContainer} />
-      <Route path="/admin/openings" component={EditOpeningsContainer} />
-      <Route path="/admin/data" />
-      <Route path="/admin/about" />
-    </Switch>
+    <React.Fragment>
+      <Login />
+      <Switch>
+        <Route path="/admin/people" component={EditPeopleContainer} />
+        <Route path="/admin/openings" component={EditOpeningsContainer} />
+        <Route path="/admin/data" />
+        <Route path="/admin/about" />
+      </Switch>
+    </React.Fragment>
   );
 };
 
