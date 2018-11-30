69763b614d0c058c766947446a5b1b2054e3426f
Dasol-Jung <xenosophian@gmail.com>
2018-11-30 21:34:15 +0900

    fetching people and openings data to PeopleAndOpeningsComponentContainer

--- client/src/components/containers/PeopleAndOpeningsContainer/PeopleAndOpeningsContainer.js
+++ client/src/components/containers/PeopleAndOpeningsContainer/PeopleAndOpeningsContainer.js
@@ -3,13 +3,47 @@ import FbApp from "../../../config/firebase";
 import { Switch, Route } from "react-router-dom";
 
 const db = FbApp.firestore();
+
 db.settings({
   timestampsInSnapshots: true
 });
 
 class PeopleAndOpeningsContainer extends React.Component {
+  state = {
+    isPeopleLoaded: false,
+    isOpeningsLoaded: false,
+    people: [],
+    openings: []
+  };
+
+  componentDidMount() {
+    db.collection("openings")
+      .orderBy("timeCreated", "desc")
+      .get()
+      .then(collection => {
+        const openings = collection.docs.map(doc => {
+          return { ...doc.data(), id: doc.id };
+        });
+        this.setState({ openings: openings, isOpeningsLoaded: true });
+      });
+
+    db.collection("people")
+      .orderBy("timeCreated", "desc")
+      .get()
+      .then(collection => {
+        const people = collection.docs.map(doc => {
+          return { ...doc.data(), id: doc.id };
+        });
+        this.setState({ people: people, isPeopleLoaded: true });
+      });
+  }
   render() {
-    return <div>People And Openings</div>;
+    return (
+      <Switch>
+        <Route path="/peopleandopenings" />
+        <Route path="/peopleandopenings/:id" />
+      </Switch>
+    );
   }
 }
 
