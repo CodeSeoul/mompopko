6052d52437ddda6b241f350c8ae5bbc47c626a0c
Dasol-Jung <xenosophian@gmail.com>
2018-12-01 11:34:19 +0900

    showing story cards

--- client/src/components/presentational/Story/Story.js
+++ client/src/components/presentational/Story/Story.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import OpeningStyle from "../../../styles/presentational/OpeningStyle.js";
+import StoryStyle from "../../../styles/components/containers/StoryStyle/StoryStyle";
 import { Link } from "react-router-dom";
 
 class Story extends Component {
@@ -10,24 +10,25 @@ class Story extends Component {
     console.log(story);
 
     return (
-      <OpeningStyle>
+      <StoryStyle>
         <Link to={`/stories/${story.id}`}>
-          <div className="opening-card">
+          <div className="story-card">
             <img
               alt=""
               src="https://cdn.pixabay.com/photo/2018/04/05/23/30/nature-3294543_960_720.jpg"
             />
             <div className="description">
-              <div id="opening-category">
+              <div id="story-category">
                 <i className="fas fa-user" />
                 {story.business.category}
               </div>
               <br />
-              <div id="opening-businessName">{story.business.businessName}</div>
+              <div id="story-businessName">{story.business.businessName}</div>
             </div>
           </div>
         </Link>
-      </OpeningStyle>
+        \{" "}
+      </StoryStyle>
     );
   }
 }
