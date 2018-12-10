0c538f5f2ee815dc8b4da8c30599848aa6f58c7a
Dasol-Jung <xenosophian@gmail.com>
2018-12-10 23:06:16 +0900

    creating story schema with different levels

---
+++ models/Lv1Stories.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongooese");
+const Schema = mongoose.Schema;
+
+// Create Schema
+
+const Lv1StorySchema = new Schema({
+  category: {
+    type: String,
+    required: true
+  },
+  name: { type: String, required: true },
+  newAddress: { type: String },
+  oldAddress: { type: String },
+  telephone: { type: String, required: true },
+  openingDate: { type: Date },
+  newZipcode: { type: String },
+  oldZipcode: { type: String },
+  type: { type: String, required: true },
+  createdDate: { type: Date, default: Date.now }
+});
+
+module.exports = Lv1Story = mongoose.model("lv1story", Lv1StorySchema);
