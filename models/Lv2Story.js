06e4133a4b850785ed3ba1daef14695e6ce3be5c
Dasol-Jung <xenosophian@gmail.com>
2018-12-10 23:19:25 +0900

    restructuring story schema

--- models/Lv2Story.js
+++ models/Lv2Story.js
@@ -4,20 +4,27 @@ const Schema = mongoose.Schema;
 // Create Schema
 
 const Lv2StorySchema = new Schema({
-  category: {
-    type: String,
-    required: true
+  business: {
+    category: {
+      type: String,
+      required: true
+    },
+    name: { type: String, required: true },
+    newAddress: { type: String },
+    oldAddress: { type: String },
+    telephone: { type: String, required: true },
+    openingDate: { type: Date },
+    newZipcode: { type: String },
+    oldZipcode: { type: String },
+    type: { type: String, required: true }
   },
-  name: { type: String, required: true },
-  newAddress: { type: String },
-  oldAddress: { type: String },
-  telephone: { type: String, required: true },
-  openingDate: { type: Date },
-  newZipcode: { type: String },
-  oldZipcode: { type: String },
-  type: { type: String, required: true },
-  interview: { type: String, required: true },
-  createdDate: { type: Date, default: Date.now }
+
+  createdDate: { type: Date, default: Date.now },
+
+  owner: {
+    name: { type: String },
+    interview: { type: String, required: true }
+  }
 });
 
 module.exports = Lv2Story = mongoose.model("lv2story", Lv2StorySchema);
