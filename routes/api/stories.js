3f137cab45a3ef530520e70e8ff5025abf13ad0c
Dasol-Jung <xenosophian@gmail.com>
2018-12-10 23:14:30 +0900

    creating a page where you can register stories

--- routes/api/stories.js
+++ routes/api/stories.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const Lv1Story = require("../../models/Lv1Story");
+const Lv2Story = require("../../models/Lv2Story");
+const Lv3Story = require("../../models/Lv3Story");
 
 //@route GET api/stories/test
 //@desc test stories page
@@ -7,6 +10,30 @@ const router = express.Router();
 
 router.get("/test", (req, res) => {
   res.json({ msg: "Stories page works" });
+});
+
+//@route POST api/stories/lv1/register
+//@desc register lv1 story
+//@access Private
+
+router.post("/lv1/register", (req, res) => {
+  Lv1Story.findOne({ name: req.body.name });
+});
+
+//@route POST api/stories/lv2/register
+//@desc register lv2 story
+//@access Private
+
+router.post("/lv2/register", (req, res) => {
+  Lv2Story.findOne({ name: req.body.name });
+});
+
+//@route POST api/stories/lv3/register
+//@desc register lv3 story
+//@access Private
+
+router.post("/lv3/register", (req, res) => {
+  Lv3Story.findOne({ name: req.body.name });
 });
 
 module.exports = router;
