9553d15064073a2df3b2609f02e32156fb10e63c
Dasol-Jung <xenosophian@gmail.com>
2018-12-11 19:21:07 +0900

    handling admin login request

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const Admin = require("../../models/Admin");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 
 //@route GET api/users/test
 //@desc test users page
@@ -38,6 +40,49 @@ router.post("/register", (req, res) => {
         });
       });
     }
+  });
+});
+
+//@route POST "/api/admin/login"
+//@desc admin login page
+//@access Public
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find admin
+
+  Admin.findOne({ email }).then(admin => {
+    if (!admin) {
+      return res.status(404).json({ email: "Email Not Found" });
+    }
+
+    // Check Password
+
+    bcrypt.compare(password, admin.password).then(isMatch => {
+      if (isMatch) {
+        // User Matched
+
+        const payload = { id: user.id, email: user.email };
+
+        // Sign Token
+
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        return res.status(400).json({ password: "Password Incorrect" });
+      }
+    });
   });
 });
 
