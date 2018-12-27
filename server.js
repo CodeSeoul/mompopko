89624638350a765a670f0806aa4b71542820258b
Dasol-Jung <xenosophian@gmail.com>
2018-12-27 18:11:22 +0900

    CORS error fix

--- server.js
+++ server.js
@@ -17,11 +17,17 @@ app.use(bodyParser.json());
 //CORS
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  //allow cross origin requests
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "POST, PUT, OPTIONS, DELETE, GET"
+  );
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Credentials", true);
   next();
 });
 
