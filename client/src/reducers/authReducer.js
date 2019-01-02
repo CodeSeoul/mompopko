80ed2ef892f895517846111f6abbe72fe5b69e68
Dasol-Jung <xenosophian@gmail.com>
2019-01-03 07:47:28 +0900

    formatting files #2

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,29 +1,24 @@
-import {
-  LOGIN,
-  LOGOUT
-} from '../actions/types'
+import { LOGIN, LOGOUT } from "../actions/types";
 
 const initialState = {
   inAuthenticated: false
-}
+};
 
-export default function (state = initialState, action) {
-
+export default function(state = initialState, action) {
   switch (action.type) {
     case LOGIN:
       return {
         ...state,
         isAuthenticated: true
-      }
+      };
 
     case LOGOUT:
       return {
         ...state,
         isAuthenticated: false
-      }
+      };
 
     default:
       return state;
   }
-
 }
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,8 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducer';
+import { combineReducers } from "redux";
+import authReducer from "./authReducer";
 
 const rootReducer = combineReducers({
-  auth : authReducer
+  auth: authReducer
 });
 
 export default rootReducer;
