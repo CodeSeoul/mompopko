319702733c7905a7c100b671a3b31128ec7efd99
Dasol <xenosophian@gmail.com>
2019-01-02 21:21:49 +0900

    redux setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import GlobalStyle from "../styles/globalStyle";
 import Main from "./Main.js";
+import { Provider } from 'react-redux';
+import store from '../store';
 
 const App = () => {
+  
   return (
-    <React.Fragment>
-      <GlobalStyle />
-      <Main />
-    </React.Fragment>
+    <Provider store={store}>
+      <React.Fragment>
+        <GlobalStyle />
+        <Main />
+      </React.Fragment>
+    </Provider>
   );
 };
 
---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  inAuthenticated : false
+}
+
+export default function(state=initialState,action) {
+
+  switch(action.type){
+    default:
+      return state;
+  }
+
+}
