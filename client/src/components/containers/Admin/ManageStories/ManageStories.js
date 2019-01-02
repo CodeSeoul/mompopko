8a534b46fcce078fa8c6cc98b1ff05b5efa20929
Dasol-Jung <xenosophian@gmail.com>
2019-01-02 17:02:09 +0900

    fetching stories data in admin page

--- client/src/components/containers/Admin/ManageStories/ManageStories.js
+++ client/src/components/containers/Admin/ManageStories/ManageStories.js
@@ -4,7 +4,10 @@ import AddStories from "./AddStories";
 import axios from "axios";
 
 class ManageStories extends Component {
-  state = {};
+  state = {
+    isLoaded: false,
+    stories: []
+  };
 
   componentDidMount() {
     axios
@@ -18,13 +21,55 @@ class ManageStories extends Component {
       });
   }
   render() {
-    console.log(this.props.stories.level1);
+    console.log(this.state.stories);
+    const tableData = this.state.stories.map(story => {
+      return (
+        <tr>
+          <td>{story.business.name}</td>
+          <td>{story.createdDate}</td>
+          <td>{story.owner.name}</td>
+          <td>
+            <Link to={`/admin/stories/${story._id}`}>
+              <button>Edit</button>
+            </Link>
+          </td>
+          <td>
+            <button onClick={() => this.deleteHandler(story._id)}>
+              Delete
+            </button>
+          </td>
+        </tr>
+      );
+    });
     return (
       <div className="manage-stories">
         <Link id="add-stories-button" to="/admin/stories/add">
           Add
         </Link>
         <Switch>
+          <Route
+            exact
+            path="/admin/stories"
+            render={() => {
+              return (
+                <div>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>Business Name</th>
+                        <th>Created Date</th>
+                        <th>Owner Name</th>
+                        <th>Level</th>
+                        <th>Delete</th>
+                        <th>Edit</th>
+                      </tr>
+                    </thead>
+                    <tbody>{tableData}</tbody>
+                  </table>
+                </div>
+              );
+            }}
+          />
           <Route
             path="/admin/stories/add"
             render={() => {
