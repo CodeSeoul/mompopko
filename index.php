c315a44cb16c4db56bde0d4527bb9d5cfbc4867d
Dasol-Jung <xenosophian@gmail.com>
2019-01-18 08:21:40 +0900

    importing header and footer php files into index.php

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>MomPopKo</title>
-</head>
-<body>
+<?php include 'html/header.php'?>
 
 <?php 
     echo "Mompopko";
 ?>
-    
-</body>
-</html>
+
+<?php include 'html/footer.php'?>
