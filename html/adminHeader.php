4d39b1cef61ae2e45e2a0e279354203c6d6926f3
Dasol-Jung <xenosophian@gmail.com>
2019-01-19 13:27:13 +0900

    changed admin header style

--- html/adminHeader.php
+++ html/adminHeader.php
@@ -8,21 +8,22 @@
     <link rel='stylesheet' href='../lib/frontend/bootstrap/css/bootstrap.css'>
 </head>
 <body>
+    <nav class="navbar navbar-light bg-light justify-content-center border-bottom navbar-expand-sm">
+    <ul class="navbar-nav justify-content-between d-flex col-8">
 
-    <ul class="nav border-bottom justify-content-center text-center">
-
-        <li class="nav-item col-6">
+        <li class="nav-item">
             <a href="#" class="nav-link">
             Businesses
             </a>
         </li>
 
-        <li class="nav-item col-6">
+        <li class="nav-item">
             <a href="#" class="nav-link">
             Data Trends
             </a>
         </li>
     
     </ul>
+    </nav>
 
     
