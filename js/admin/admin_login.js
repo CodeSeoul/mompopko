dd10fb7c87d84cd761c0f08f034b716f74fa70e2
Dasol-Jung <xenosophian@gmail.com>
2019-01-29 16:03:08 +0900

    admin_login javascript

---
+++ js/admin/admin_login.js
@@ -0,0 +1,27 @@
+function login(e) {
+
+    e.preventDefault();
+    let form = e.target;
+    let id = form.elements.id.value;
+    let password = form.elements.password.value;
+    let data = {
+        id: id,
+        password: password
+    }
+
+    fetch("../../java/adminLogin.java", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json()).then(data => {
+        console.log(JSON.stringify(data))
+    }).catch(err => {
+        console.log(err)
+    })
+    console.log(data)
+}
+
+let loginForm = document.querySelector("#login-form");
+loginForm.addEventListener('submit', (e) => login(e))
