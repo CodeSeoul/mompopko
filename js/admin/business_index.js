94151030508b1e6fd35834f805dc64441255adc0
Dasol-Jung <xenosophian@gmail.com>
2019-01-29 22:10:56 +0900

    making load, search, page functionality

--- js/admin/business_index.js
+++ js/admin/business_index.js
@@ -1,55 +1,128 @@
-function addRow(number) {
-  let tr = document.createElement("tr");
-  let thCheckbox = document.createElement("th");
-  let thNumber = document.createElement("th");
-  let tdName = document.createElement("td");
-  let tdLevel = document.createElement("td");
-  let tdPhone = document.createElement("td");
-  let tdAddress = document.createElement("td");
-  let tdEdit = document.createElement("td");
-  let tdImage = document.createElement("td");
-  let tdStatus = document.createElement("td");
-  let form = document.createElement("form");
-  let checkbox = document.createElement("input");
-  let buttonEdit = document.createElement("button");
-  let buttonImage = document.createElement("button");
-  let childrenArr = [];
-  let tbody = document.querySelector("tbody");
+//fetchData from server
 
-  thCheckbox.scope = "row";
-  checkbox.type = "checkbox";
-  thNumber.scope = "row";
-  buttonEdit.className = "btn btn-warning";
-  buttonImage.className = "btn btn-info";
+function fetchData(
+  searchKey = "",
+  pageIndex = 0,
+  postPerPage = 20,
+  sort = "businessName",
+  ascending = true
+) {
+  const data = {};
+  data.searchKey = searchKey;
+  data.pageIndex = pageIndex;
+  data.postPerPage = postPerPage;
+  data.sort = sort;
+  data.ascending = ascending;
 
-  form.appendChild(checkbox);
-  thCheckbox.appendChild(form);
-  childrenArr.push(thCheckbox);
-  thNumber.appendChild(document.createTextNode(number));
-  childrenArr.push(thNumber);
-  tdName.appendChild(document.createTextNode("Name"));
-  childrenArr.push(tdName);
-  tdLevel.appendChild(document.createTextNode("Level"));
-  childrenArr.push(tdLevel);
-  tdPhone.appendChild(document.createTextNode("Phone"));
-  childrenArr.push(tdPhone);
-  tdAddress.appendChild(document.createTextNode("Address"));
-  childrenArr.push(tdAddress);
-  buttonEdit.appendChild(document.createTextNode("Edit"));
-  tdEdit.appendChild(buttonEdit);
-  childrenArr.push(tdEdit);
-  buttonImage.appendChild(document.createTextNode("Select"));
-  tdImage.appendChild(buttonImage);
-  childrenArr.push(tdImage);
-  tdStatus.appendChild(document.createTextNode("-"));
-  childrenArr.push(tdStatus);
-
-  for (let i = 0; i < childrenArr.length; i++) {
-    tr.appendChild(childrenArr[i]);
-  }
-
-  tbody.appendChild(tr);
+  fetch("../../java/business_index", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log(JSON.stringify(data));
+      return { meta: data, businesses: JSON.stringify(data) };
+    })
+    .catch(err => {
+      console.log(err);
+    });
 }
+
+//show business data in table body
+
+function loadRow(data) {
+  let meta = data["meta"];
+  let businesses = JSON.parse(data["businesses"]);
+  let index = 0;
+  for (business in businesses) {
+    let tr = document.createElement("tr");
+    let thCheckbox = document.createElement("th");
+    let thNumber = document.createElement("th");
+    let tdName = document.createElement("td");
+    let tdLevel = document.createElement("td");
+    let tdPhone = document.createElement("td");
+    let tdAddress = document.createElement("td");
+    let tdEdit = document.createElement("td");
+    let tdImage = document.createElement("td");
+    let tdStatus = document.createElement("td");
+    let form = document.createElement("form");
+    let checkbox = document.createElement("input");
+    let buttonEdit = document.createElement("button");
+    let buttonImage = document.createElement("button");
+    let childrenArr = [];
+    let tbody = document.querySelector("tbody");
+
+    thCheckbox.scope = "row";
+    checkbox.type = "checkbox";
+    thNumber.scope = "row";
+    buttonEdit.className = "btn btn-warning";
+    buttonImage.className = "btn btn-info";
+
+    form.appendChild(checkbox);
+    thCheckbox.appendChild(form);
+    childrenArr.push(thCheckbox);
+    thNumber.appendChild(
+      document.createTextNode(meta.pageIndex * meta.postPerPage) + index
+    );
+    childrenArr.push(thNumber);
+    tdName.appendChild(document.createTextNode(business["businessName"]));
+    childrenArr.push(tdName);
+    tdLevel.appendChild(document.createTextNode(business["level"]));
+    childrenArr.push(tdLevel);
+    tdPhone.appendChild(document.createTextNode(business["tel"]));
+    childrenArr.push(tdPhone);
+    tdAddress.appendChild(document.createTextNode(business["address"]));
+    childrenArr.push(tdAddress);
+    buttonEdit.appendChild(document.createTextNode("Edit"));
+    tdEdit.appendChild(buttonEdit);
+    childrenArr.push(tdEdit);
+    buttonImage.appendChild(document.createTextNode("Select"));
+    tdImage.appendChild(buttonImage);
+    childrenArr.push(tdImage);
+    tdStatus.appendChild(document.createTextNode("-"));
+    childrenArr.push(tdStatus);
+    index++;
+
+    for (let i = 0; i < childrenArr.length; i++) {
+      tr.appendChild(childrenArr[i]);
+    }
+
+    tbody.replaceChild(tr, tbody.lastChild);
+  }
+}
+
+// add event to page buttons
+
+function pageEvent() {
+  let pageButtons = document.querySelectorAll(".page-number");
+
+  pageButtons.forEach(pageButton =>
+    pageButton.addEventListener("click", e => changePage(e), true)
+  );
+}
+
+// change the page when admin clicks a page number
+
+function changePage(e) {
+  let pageButtons = document.querySelectorAll(".page-number");
+  pageButtons.forEach(pageButton => {
+    pageButton.classList.remove("active");
+  });
+  e.target.parentNode.classList.toggle("active");
+  loadRow(
+    fetchData(
+      document.querySelector("#search-button").elements.search.value,
+      parseInt(e.target.textContent)
+    )
+  );
+}
+
+//previous, next page buttons' functions has yet to be created
+
+//check or uncheck all checkboxes
 
 function checkAllCheckboxes() {
   let checkAll = document.querySelector("#check-all");
@@ -74,42 +147,31 @@ function checkAllCheckboxes() {
   });
 }
 
-function statusUpdate() {
-  let checkboxes = document.querySelectorAll("tbody input[type='checkbox']");
-
-  for (let i = 0; i < checkboxes.length; i++) {
-    let status =
-      checkboxes[i].parentNode.parentNode.parentNode.lastChild.lastChild;
-    checkboxes[i].addEventListener("change", e => {
-      if (e.target.checked) {
-        status.nodeValue = "Delete";
-      } else {
-        status.nodeValue = "-";
-      }
-    });
-  }
-}
-
+// send a request and show the results when admin searches something
 function searchEnter(e) {
   e.preventDefault();
-  console.log("hey");
+  loadRow(fetchData(e.target.elements.search.value));
 }
+
+//add submit event to search bar
 
 function searchEvent() {
   let searchButton = document.querySelector("#search-button");
   searchButton.addEventListener("submit", e => searchEnter(e));
 }
 
+//init
+pageEvent();
+loadRow(fetchData());
+
 /*
  **      EXECUTION CONTENT      **
  **      EXECUTION EVENTS       **
  */
 
-for (let i = 0; i < 20; i++) {
-  addRow(i);
-}
 statusUpdate();
 checkAllCheckboxes();
+
 searchEvent();
 
 /*
