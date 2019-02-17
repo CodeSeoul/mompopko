fe76eef11625409c317a829cb5e18b6e23158e1e
david <thyun.ahn@gmail.com>
2019-02-17 21:53:23 +0900

    adding logic for load menu data in menu popup

--- js/admin/business_edit.js
+++ js/admin/business_edit.js
@@ -19,14 +19,90 @@ const menuBtnElem = document.getElementById("menuBtn");
  * ----------------------------------------------------------------------------------
  */
 const menuSelectPopupElem = () => {
+  // example for menu data (until load from DB)
+  const objMenu = [
+    {
+      menuId: "1",
+      menuName: "Food&Drink",
+      menuPageYn: "0"
+    },
+    {
+      menuId: "2",
+      menuName: "Restaurants",
+      menuPageYn: "0"
+    },
+    {
+      menuId: "3",
+      menuName: "Korean",
+      menuPageYn: "1"
+    },
+    {
+      menuId: "4",
+      menuName: "Japanese",
+      menuPageYn: "1"
+    },
+    {
+      menuId: "43",
+      menuName: "Services",
+      menuPageYn: "0"
+    },
+    {
+      menuId: "44",
+      menuName: "Accommodations",
+      menuPageYn: "1"
+    }
+  ];
+
   let divElem = document.createElement("div");
-  divElem.innerHTML = "testing for popup";
+  divElem.innerHTML = `
+  <table class="table-bordered text-center table table-striped">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">
+        </th>
+        <th scope="col">#</th>
+        <th scope="col">MenuName</th>
+        <th scope="col">MenuId</th>
+        <th scope="col">Has Page</th>
+      </tr>
+    </thead>
+    <tbody id="menu-popup-tbody>
+    </tbody>
+  </table>`;
+
+  let arryTbody = [];
+  let objTbodyTd = {};
+  let tbodyMenuElem = objMenu.map((menu) => {
+    tbodyTrElem = document.createElement("tr");
+    objTbodyTd.tbodyTdRowNum = document.createElement("td");
+    objTbodyTd.tbodyTdRowNum.appendChild(document.createTextNode(1));
+    objTbodyTd.tbodyTdMenuName = document.createElement("td");
+    objTbodyTd.tbodyTdMenuName.appendChild(
+      document.createTextNode(menu.menuName)
+    );
+    objTbodyTd.tbodyTdMenuId = document.createElement("td");
+    objTbodyTd.tbodyTdMenuId.appendChild(document.createTextNode(menu.menuId));
+    objTbodyTd.tbodyTdHasPage = document.createElement("td");
+    objTbodyTd.tbodyTdHasPage.appendChild(
+      document.createTextNode(menu.menuPageYn)
+    );
+    objTbodyTd.tbodyThCheckbox = document.createElement("td");
+    objTbodyTd.tbodyThCheckbox.appendChild(document.createTextNode("checkbox"));
+
+    for (tdElem in objTbodyTd) {
+      tbodyTrElem.appendChild(objTbodyTd[tdElem]);
+    }
+    arryTbody.push(tbodyTrElem);
+  });
+
+  console.log(arryTbody);
+
   return divElem;
 };
 
 /**
  * ==================================================================================
- * EventListners
+ * EventListeners
  * ==================================================================================
  */
 
--- js/admin/business_index.js
+++ js/admin/business_index.js
@@ -17,10 +17,11 @@ let utils = (() => {
   //      END EXECUTION EVENTS
 
   // business Level
-  let bizLevelInfo = { level1: 1, level2: 2, level3: 3 };
+  const bizLevelInfo = { level1: 1, level2: 2, level3: 3 };
+  // boolean Information
+  const booleanInfo = { 0: false, 1: true };
 
   //business delete list
-
   let businessToDelete = [];
 
   /**
