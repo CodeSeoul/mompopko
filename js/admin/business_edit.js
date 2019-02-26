1725294372a338b9ea9408c11c881ba8bfe41662
david <thyun.ahn@gmail.com>
2019-02-26 10:51:21 +0900

    1. modify SQL for business edit, 2. modify column name from operating-hour to opening-hour

--- js/admin/business_edit.js
+++ js/admin/business_edit.js
@@ -12,82 +12,101 @@ const menuBtnElem = document.getElementById("menuBtn");
  * ==================================================================================
  */
 
- /**
- * 
+/**
+ *
  * -----------------------------------------------------------------------
  * prefillBusinessForm : prefill form with selected business' data
  * -----------------------------------------------------------------------
- * 
+ *
  */
 
- (function prefillBuinessForm(){
-  if(localStorage.getItem("selectedBusiness")!=null){
-
+(function prefillBuinessForm() {
+  if (localStorage.getItem("selectedBusiness") != null) {
     let business = JSON.parse(localStorage.getItem("selectedBusiness"));
-    for(key in business){
+    for (key in business) {
       console.log(key, "\n");
     }
 
     //fetch Image URL for preview
 
     let xhr = new XMLHttpRequest();
-    xhr.open('POST','../../php/imgURLFinder.php');
-    xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function(){
-      if(xhr.readyState!=4) return;
-      if(xhr.status>=200 && xhr.status<300){
+    xhr.open("POST", "../../php/imgURLFinder.php");
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState != 4) return;
+      if (xhr.status >= 200 && xhr.status < 300) {
         let images = JSON.parse(xhr.responseText);
         createImageHandler(images);
-      }else{
+      } else {
         console.log(xhr.response);
       }
-    }
-    xhr.send(`file_grp_id=${business['file_grp_id']}`);
+    };
+    xhr.send(`file_grp_id=${business["file_grp_id"]}`);
 
-    document.querySelector('select[name="bizLevel"]').value = business['biz_level'] || '';
-    document.querySelector('input[name="menuId"]').value = business['menu_id'] || '';
-    document.querySelector('input[name="menuName"]').value = business['menu_name'] || '';
-    document.querySelector('input[name="openingDate"]').value = business['biz_open_date'] || '';
-    document.querySelector('input[name="newAddress"]').value = business['biz_address'] || '';
-    document.querySelector('input[name="province"]').value = business['biz_province'] || '';
-    document.querySelector('input[name="city"]').value = business['biz_city'] || '';
-    document.querySelector('input[name="district"]').value = business['biz_district'] || '';
-    document.querySelector('input[name="neighborhood"]').value = business['biz_neighborhood'] || '';
-    document.querySelector('input[name="newZipcode"]').value = business['biz_zipcode'] || '';
-    document.querySelector('input[name="tel"]').value = business['biz_tel'] || '';
-    document.querySelector('input[name="email"]').value = business['biz_email'] || '';
-    document.querySelector('input[name="operatingHour"]').value = business['biz_operatingHour'] || '';
-    document.querySelector('input[name="ownerName"]').value = business['biz_owner'] || '';
-    document.querySelector('textarea[name="interview"]').value = business['biz_interview_conts'] || '';
-    document.querySelector('input[name="interviewDate"]').value = business['biz_interview_date'] || '';
-    document.querySelector('input[name="website"]').value = business['biz_website'] || '';
-    document.querySelector('input[name="facebook"]').value = business['biz_facebook'] || '';
-    document.querySelector('input[name="instagram"]').value = business['biz_instagram'] || '';
-    document.querySelector('input[name="youtube"]').value = business['biz_youtube'] || '';
-    document.querySelector('input[name="twitter"]').value = business['biz_twitter'] || '';
+    document.querySelector('select[name="bizLevel"]').value =
+      business["biz_level"] || "";
+    document.querySelector('input[name="menuId"]').value =
+      business["menu_id"] || "";
+    document.querySelector('input[name="menuName"]').value =
+      business["menu_name"] || "";
+    document.querySelector('input[name="openingDate"]').value =
+      business["biz_open_date"] || "";
+    document.querySelector('input[name="newAddress"]').value =
+      business["biz_address"] || "";
+    document.querySelector('input[name="province"]').value =
+      business["biz_province"] || "";
+    document.querySelector('input[name="city"]').value =
+      business["biz_city"] || "";
+    document.querySelector('input[name="district"]').value =
+      business["biz_district"] || "";
+    document.querySelector('input[name="neighborhood"]').value =
+      business["biz_neighborhood"] || "";
+    document.querySelector('input[name="newZipcode"]').value =
+      business["biz_zipcode"] || "";
+    document.querySelector('input[name="tel"]').value =
+      business["biz_tel"] || "";
+    document.querySelector('input[name="email"]').value =
+      business["biz_email"] || "";
+    document.querySelector('input[name="openingHour"]').value =
+      business["biz_open_hour"] || "";
+    document.querySelector('input[name="ownerName"]').value =
+      business["biz_owner"] || "";
+    document.querySelector('textarea[name="interview"]').value =
+      business["biz_interview_conts"] || "";
+    document.querySelector('input[name="interviewDate"]').value =
+      business["biz_interview_date"] || "";
+    document.querySelector('input[name="website"]').value =
+      business["biz_website"] || "";
+    document.querySelector('input[name="facebook"]').value =
+      business["biz_facebook"] || "";
+    document.querySelector('input[name="instagram"]').value =
+      business["biz_instagram"] || "";
+    document.querySelector('input[name="youtube"]').value =
+      business["biz_youtube"] || "";
+    document.querySelector('input[name="twitter"]').value =
+      business["biz_twitter"] || "";
   }
- })();
+})();
 
-  /**
- * 
+/**
+ *
  * -----------------------------------------------------------------------
  * createImageHandler : create a HTML element where you can replace existing images or add new ones
  * -----------------------------------------------------------------------
- * 
+ *
  */
 
-function createImageHandler(imagesObj){
-
+function createImageHandler(imagesObj) {
   // input to add new images
 
-  let container = document.createElement('div');
+  let container = document.createElement("div");
   container.classList.add("imageHandler-container");
 
   // create input element of type 'file'
   let addImagesInput = document.createElement("input");
-  addImagesInput.name = 'newSubImages';
-  addImagesInput.id = 'newSubImages';
-  addImagesInput.type = 'file';
+  addImagesInput.name = "newSubImages";
+  addImagesInput.id = "newSubImages";
+  addImagesInput.type = "file";
   addImagesInput.className = "add-images-input";
   addImagesInput.multiple = true;
 
@@ -97,12 +116,9 @@ function createImageHandler(imagesObj){
   addImagesLabel.for = "newSubImages";
   addImagesLabel.textContent = "Add Images";
 
-
   // append add-new-images element
   container.appendChild(addImagesLabel);
   container.appendChild(addImagesInput);
- 
-
 
   // replace existing images
 
@@ -112,26 +128,26 @@ function createImageHandler(imagesObj){
   currentImagesContainer.className = "current-images-container";
 
   // create img element with current images
-  for(index in imagesObj){
+  for (index in imagesObj) {
     let image = imagesObj[index];
     let currentImage = document.createElement("img");
-    currentImage.src = image['file_path'].replace("/var/www/html","");
+    currentImage.src = image["file_path"].replace("/var/www/html", "");
 
     let currentImageInput = document.createElement("input");
-    currentImageInput.type = 'file';
+    currentImageInput.type = "file";
     currentImageInput.hidden = true;
-    currentImageInput.id = 'newImage' + image['file_id'];
-    currentImageInput.name = 'newImage' + image['file_id'];
+    currentImageInput.id = "newImage" + image["file_id"];
+    currentImageInput.name = "newImage" + image["file_id"];
 
-    let currentImageLabel = document.createElement('label');
-    currentImageLabel.setAttribute("for",'newImage' + image['file_id']);
+    let currentImageLabel = document.createElement("label");
+    currentImageLabel.setAttribute("for", "newImage" + image["file_id"]);
     currentImageLabel.appendChild(currentImage);
-    currentImageLabel.input
+    currentImageLabel.input;
 
-    currentImageInput.addEventListener('change',()=>{
+    currentImageInput.addEventListener("change", () => {
       currentImage.src = URL.createObjectURL(currentImageInput.files[0]);
       currentImageLabel.appendChild(currentImage);
-    })
+    });
 
     currentImagesContainer.appendChild(currentImageLabel);
     currentImagesContainer.appendChild(currentImageInput);
@@ -139,7 +155,6 @@ function createImageHandler(imagesObj){
     container.appendChild(currentImagesContainer);
     document.querySelector("form").appendChild(container);
   }
-  
 }
 
 /**
@@ -159,7 +174,3 @@ menuBtnElem.addEventListener("click", () => {
     menuNameElem: document.getElementById("menuName")
   });
 });
-
-
-
-
--- php/business_index.php
+++ php/business_index.php
@@ -5,8 +5,8 @@
         
         $data = json_decode($_POST['data'], true);
         $host = 'mompopkoapi.wcoding.com';
-        $user = 'mompopko_admin';
-        $password = 'windMarshall92Adult';
+        $user = 'mompopko_api';
+        $password = 'uAVPVDMZz9N8d9RC';
         $port = '8833';
         $dbname = 'mompopko';
     
@@ -17,16 +17,83 @@
         $postPerPage = (int)$data['postPerPage'];
         $startIndex = ((int)$data['pageIndex']-1)*$postPerPage;
         
-        //search key exists
-        // param에 따라 정렬 기준(asd, desc) SQL query를 바꾸는 기능 구현 필요
-        if($data['searchKey']==''){
-            $query = 'SELECT * FROM tb_biz ORDER BY :sort LIMIT :startIndex, :numberOfPosts';
-            $req = $pdo->prepare($query);
-
         //search key doesn't exist
         // param에 따라 정렬 기준(asd, desc) SQL query를 바꾸는 기능 구현 필요
+        if($data['searchKey']==''){
+            //$query = 'SELECT * FROM tb_biz ORDER BY :sort LIMIT :startIndex, :numberOfPosts';
+            $query = 'SELECT A.biz_id
+                            ,A.menu_id
+                            ,(SELECT SA.menu_name FROM tb_menu SA WHERE SA.menu_id = A.menu_id) AS menu_name
+                            ,A.file_grp_id
+                            ,A.biz_name
+                            ,A.biz_level
+                            ,A.biz_view_cnt
+                            ,A.biz_owner
+                            ,A.biz_open_date
+                            ,A.biz_close_date
+                            ,A.biz_address
+                            ,A.biz_province
+                            ,A.biz_city
+                            ,A.biz_district
+                            ,A.biz_neighborhood
+                            ,A.biz_zipcode
+                            ,A.biz_tel
+                            ,A.biz_email
+                            ,A.biz_open_hour
+                            ,A.biz_interview_conts
+                            ,A.biz_interview_date
+                            ,A.biz_popular_item
+                            ,A.biz_recommended
+                            ,A.biz_website
+                            ,A.biz_facebook
+                            ,A.biz_instagram
+                            ,A.biz_youtube
+                            ,A.biz_twitter
+                            ,A.frst_input_date
+                            ,A.last_update_date
+                        FROM tb_biz A 
+                        ORDER BY :sort 
+                        LIMIT :startIndex, :numberOfPosts';
+            $req = $pdo->prepare($query);
+
+        //search key exists
+        // param에 따라 정렬 기준(asd, desc) SQL query를 바꾸는 기능 구현 필요
         }else{
-            $query = 'SELECT * FROM tb_biz WHERE biz_name LIKE :searchKey OR biz_tel LIKE :searchKey OR biz_address LIKE :searchKey ORDER BY :sort LIMIT :startIndex, :numberOfPosts ';
+            //$query = 'SELECT * FROM tb_biz WHERE biz_name LIKE :searchKey OR biz_tel LIKE :searchKey OR biz_address LIKE :searchKey ORDER BY :sort LIMIT :startIndex, :numberOfPosts ';
+            $query = 'SELECT A.biz_id
+                            ,A.menu_id
+                            ,(SELECT SA.menu_name FROM tb_menu SA WHERE SA.menu_id = A.menu_id) AS menu_name
+                            ,A.file_grp_id
+                            ,A.biz_name
+                            ,A.biz_level
+                            ,A.biz_view_cnt
+                            ,A.biz_owner
+                            ,A.biz_open_date
+                            ,A.biz_close_date
+                            ,A.biz_address
+                            ,A.biz_province
+                            ,A.biz_city
+                            ,A.biz_district
+                            ,A.biz_neighborhood
+                            ,A.biz_zipcode
+                            ,A.biz_tel
+                            ,A.biz_email
+                            ,A.biz_open_hour
+                            ,A.biz_interview_conts
+                            ,A.biz_interview_date
+                            ,A.biz_popular_item
+                            ,A.biz_recommended
+                            ,A.biz_website
+                            ,A.biz_facebook
+                            ,A.biz_instagram
+                            ,A.biz_youtube
+                            ,A.biz_twitter
+                            ,A.frst_input_date
+                            ,A.last_update_date
+                        FROM tb_biz A 
+                        WHERE A.biz_name LIKE :searchKey OR A.biz_tel LIKE :searchKey OR A.biz_address LIKE :searchKey 
+                        ORDER BY :sort 
+                        LIMIT :startIndex, :numberOfPosts ';
             $req = $pdo->prepare($query);
             $searchKey = "%" . $data['searchKey'] . "%";
             $req->bindParam(':searchKey', $searchKey,PDO::PARAM_STR);
