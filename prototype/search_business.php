4fbeca5c68c44e53cf7a249563688344a99d8256
david <thyun.ahn@gmail.com>
2019-02-27 12:45:19 +0900

    1.fix unmatched html tag in index.html  2.add search_business.php 3.split header.php&footer.php

--- prototype/search_business.php
+++ prototype/search_business.php
@@ -1,1 +1,42 @@
-hello
+<!DOCTYPE html>
+<html>
+    
+
+    <?php
+        include_once 'header.php';
+    ?>
+
+	<section id="content">
+		<!-- <div class="container">
+			<div class="row">
+				<div class="col-xs-12">
+					<ul class="nav nav-tabs">
+					  	<li><h3 class="title">New Openings</h3></li>
+					  	<li class="active"><a data-toggle="tab" href="#recent">[RECENT]</a></li>
+					  	<li><a data-toggle="tab" href="#popular">[POPULAR]</a></li>
+					  	<li><a data-toggle="tab" href="#trends">[TRENDS]</a></li>
+					</ul>
+				</div>
+				<div class="tab-content">
+					<div id="recent" class="tab-pane fade in active">
+					</div>
+						<div class="col-xs-12 alignright mgbottom30" id="recent_button">
+						</div>
+					</div>
+					<div id="popular" class="tab-pane fade">
+					</div>
+					<div id="trends" class="tab-pane fade">
+					</div>
+				</div>
+			</div>
+		</div> -->
+    </section>
+
+    <?php
+        include_once 'footer.php';
+    ?>
+    
+
+
+</body>
+</html>
