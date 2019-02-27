f40a0d5d8a924054ce91bd9b803dd82b3b5a7f7b
david <thyun.ahn@gmail.com>
2019-02-28 00:17:18 +0900

    add serviceUtils for module

---
+++ prototype/js/serviceUtils.js
@@ -0,0 +1,44 @@
+/**
+ * ==================================================================================
+ * serviceUtils : This is module for supply utils to all the services
+ * ==================================================================================
+ */
+const serviceUtils = (() => {
+  /**
+   * ----------------------------------------------------------------------------------
+   * searchBiz : search for business & move to search result php page
+   * @param : event of submit eventListener
+   * ----------------------------------------------------------------------------------
+   */
+  let searchBiz = (e) => {
+    e.preventDefault();
+
+    // value of search key word.
+    const searchKeyword = e.srcElement[1].value;
+
+    // message when no keywords
+    if (
+      searchKeyword === undefined ||
+      searchKeyword === null ||
+      searchKeyword == ""
+    ) {
+      alert("Please enter the search keyword");
+      return false;
+    }
+
+    // destination of file name.
+    let fileMoveTo = "search_business.php";
+
+    fileMoveTo += "?searchKeyword=" + searchKeyword;
+
+    // move to search_business.php for search result page.
+    let lastIndexOfSlash = window.location.pathname.lastIndexOf("/");
+    window.location.href =
+      window.location.pathname.substr(0, lastIndexOfSlash + 1) + fileMoveTo;
+  };
+
+  // supply utils
+  return {
+    searchBiz
+  };
+})();
