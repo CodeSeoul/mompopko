fad0d25dd1d7e24f9c6b3a2975e1e42b37507420
david <thyun.ahn@gmail.com>
2019-03-01 12:35:45 +0900

    modify DB connection info & modify SQL to bring menu_name

--- php/biz_db_recent.php
+++ php/biz_db_recent.php
@@ -2,8 +2,8 @@
     try
 {
     $host = 'mompopkoapi.wcoding.com';
-    $user = 'mompopko_admin';
-    $password = 'windMarshall92Adult';
+    $user = 'mompopko_api';
+    $password = 'uAVPVDMZz9N8d9RC';
     $port = '8833';
     $dbname = 'mompopko';
 
@@ -12,7 +12,16 @@
     $pdo = new PDO($dsn, $user, $password,array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
 
     $nb_posts = (int)$_POST['nb_posts'];
-    $req = $pdo->prepare('SELECT * FROM tb_biz LEFT JOIN tb_file ON tb_biz.file_grp_id = tb_file.file_grp_id WHERE tb_file.file_order=1 ORDER BY tb_biz.frst_input_date DESC LIMIT 0, :nb_posts');
+    $req = $pdo->prepare('SELECT * 
+                            FROM tb_biz 
+                        INNER JOIN tb_menu
+                            ON tb_menu.menu_id = tb_biz.menu_id
+                        LEFT JOIN tb_file 
+                            ON tb_biz.file_grp_id = tb_file.file_grp_id 
+                            WHERE tb_file.file_order=1 
+                        ORDER BY tb_biz.frst_input_date DESC 
+                        LIMIT 0, :nb_posts'
+                        );
     $req->bindParam(":nb_posts",$nb_posts,PDO::PARAM_INT);
     $req->execute();
     $result = $req -> fetchAll();
