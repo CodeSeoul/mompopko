1a1962d8087e77b10d08c54ae0e2166e7cfd630f
Dasol Jung <xenosophian@gmail.com>
2019-03-02 11:01:04 +0900

    add parent menu to loadByPopular function

--- php/biz_db_popular.php
+++ php/biz_db_popular.php
@@ -12,7 +12,8 @@
     $pdo = new PDO($dsn, $user, $password,array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
 
     $nb_posts = (int)$_POST['nb_posts'];
-    $req = $pdo->prepare('SELECT * FROM tb_biz LEFT JOIN tb_file ON tb_biz.file_grp_id = tb_file.file_grp_id WHERE tb_file.file_order=1 ORDER BY tb_biz.biz_view_cnt DESC LIMIT 0, :nb_posts');
+    $req = $pdo->prepare('SELECT *, fn_menu_parents(tb_biz.menu_id) AS menu_parents FROM tb_biz INNER JOIN tb_menu
+    ON tb_menu.menu_id = tb_biz.menu_id LEFT JOIN tb_file ON tb_biz.file_grp_id = tb_file.file_grp_id WHERE tb_file.file_order=1 ORDER BY tb_biz.biz_view_cnt DESC LIMIT 0, :nb_posts');
     $req->bindParam(":nb_posts",$nb_posts,PDO::PARAM_INT);
     $req->execute();
     $result = $req -> fetchAll();
