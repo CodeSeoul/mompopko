436bfa464b6953b51f2d409ce753e090d0eb2e55
Jun <xjun852x@hotmail.com>
2019-03-02 10:37:08 +0900

    added food&drinks korean

--- php/biz_db_recent.php
+++ php/biz_db_recent.php
@@ -6,17 +6,23 @@
     $password = 'windMarshall92Adult';
     $port = '8833';
     $dbname = 'mompopko';
-
     $dsn = 'mysql:host='. $host . ';port=' . $port . ';dbname=' . $dbname;
-
     $pdo = new PDO($dsn, $user, $password,array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
-
     $nb_posts = (int)$_POST['nb_posts'];
-    $req = $pdo->prepare('SELECT * FROM tb_biz LEFT JOIN tb_file ON tb_biz.file_grp_id = tb_file.file_grp_id WHERE tb_file.file_order=1 ORDER BY tb_biz.frst_input_date DESC LIMIT 0, :nb_posts');
+    $req = $pdo->prepare('SELECT *
+                                ,fn_menu_parents(tb_biz.menu_id) AS menu_parents
+                            FROM tb_biz 
+                        INNER JOIN tb_menu
+                            ON tb_menu.menu_id = tb_biz.menu_id
+                        LEFT JOIN tb_file 
+                            ON tb_biz.file_grp_id = tb_file.file_grp_id 
+                            WHERE tb_file.file_order=1 
+                        ORDER BY tb_biz.frst_input_date DESC 
+                        LIMIT 0, :nb_posts'
+                        );
     $req->bindParam(":nb_posts",$nb_posts,PDO::PARAM_INT);
     $req->execute();
     $result = $req -> fetchAll();
-
     echo json_encode($result);
 }
     catch (Exception $e)
--- public/js/index.js
+++ public/js/index.js
@@ -15,17 +15,17 @@
  */
 
 (function() {
-	document.querySelector('#recent-button').addEventListener('click', () => {
-		loadByRecent(3);
-	});
+  document.querySelector("#recent-button").addEventListener("click", () => {
+    loadByRecent(3);
+  });
 
-	document.querySelector('#popular-button').addEventListener('click', () => {
-		loadByPopular(3);
-	});
+  document.querySelector("#popular-button").addEventListener("click", () => {
+    loadByPopular(3);
+  });
 
-	document.querySelector('#trends-button').addEventListener('click', () => {
-		loadTrends(3);
-	});
+  document.querySelector("#trends-button").addEventListener("click", () => {
+    loadTrends(3);
+  });
 })();
 
 /**
@@ -38,33 +38,43 @@
  */
 
 function loadByRecent(nb_posts) {
-	var ajax_biz = new XMLHttpRequest();
-	ajax_biz.open('POST', 'php/biz_db_recent.php', true);
-	ajax_biz.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	ajax_biz.send(`nb_posts=${nb_posts}`);
-	ajax_biz.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			var result = JSON.parse(this.responseText);
-			var offset = nb_posts;
-			var main_div_recent = document.querySelector('#recent');
-			main_div_recent.innerHTML = '';
+  var ajax_biz = new XMLHttpRequest();
+  ajax_biz.open("POST", "php/biz_db_recent.php", true);
+  ajax_biz.setRequestHeader(
+    "Content-Type",
+    "application/x-www-form-urlencoded"
+  );
+  ajax_biz.send(`nb_posts=${nb_posts}`);
+  ajax_biz.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var result = JSON.parse(this.responseText);
+      var offset = nb_posts;
+      var main_div_recent = document.querySelector("#recent");
+      main_div_recent.innerHTML = "";
+      console.log(result);
+      for (var i = 0; i < result.length && i < offset; i++) {
+        var biz_name = result[i].biz_name;
+        var biz_level = result[i].biz_level;
+        var biz_id = result[i].biz_id;
+        var menu_level = result[i].menu_level;
 
-			for (var i = 0; i < result.length && i < offset; i++) {
-				var biz_name = result[i].biz_name;
-				var biz_level = result[i].biz_level;
-				var biz_id = result[i].biz_id;
-
-				main_div_recent.innerHTML += `<div class="col-xs-12 col-sm-6 col-md-4 business-post">
+        var menu_main = result[i].menu_name;
+        var arry_menu_parents = result[i].menu_parents.split(" > ");
+        var arry_topest_parent = arry_menu_parents[0];
+        main_div_recent.innerHTML += `<div class="col-xs-12 col-sm-6 col-md-4 business-post">
 					<div class="thumb-box">
 						<div class="thumb-img">
-							<a href="php/mamalee_level_${biz_level}.php?biz_id=${biz_id}">
-								<img src=${result[i]['file_path'].replace('/var/www/html/', 'public/')} width="100%" alt="" />
+							<a href="php/mamalee_level_${biz_level}.php?biz_id=${biz_id}" id="${biz_name}">
+								<img src=${result[i]["file_path"].replace(
+                  "/var/www/html/",
+                  "public/"
+                )} width="100%" alt="" />
 							</a>
 						</div>
 						<div class="thumb-content">
 							<h5 class="thumb-category">
-								<span class="main" class="menu_zero_span"></span>
-								<span class="sub" class="menu_sub_cat">Korean</span>
+								<span class="main" class="menu_zero_span">${arry_topest_parent}</span>
+								<span class="sub" class="menu_sub_cat">${menu_main}</span>
 							</h5>
 							<span class="img_two"><span/>
 								<div class="row">
@@ -80,13 +90,13 @@ function loadByRecent(nb_posts) {
 						</div>
 					</div>
 				</div>`;
-			}
+      }
 
-			var recent_button = document.querySelector('#recent_button');
-			recent_button.innerHTML = `<button class="btn btn-more" data-toggle="collapse" data-target="#moreThumb">See More</button>`;
-			loadMore('recent');
-		}
-	};
+      var recent_button = document.querySelector("#recent_button");
+      recent_button.innerHTML = `<button class="btn btn-more" data-toggle="collapse" data-target="#moreThumb">See More</button>`;
+      loadMore("recent");
+    }
+  };
 }
 
 /**
@@ -99,28 +109,34 @@ function loadByRecent(nb_posts) {
  */
 
 function loadByPopular(nb_posts) {
-	ajax_pop = new XMLHttpRequest();
-	ajax_pop.open('POST', 'php/biz_db_popular.php', true);
-	ajax_pop.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	ajax_pop.send(`nb_posts=${nb_posts}`);
+  ajax_pop = new XMLHttpRequest();
+  ajax_pop.open("POST", "php/biz_db_popular.php", true);
+  ajax_pop.setRequestHeader(
+    "Content-Type",
+    "application/x-www-form-urlencoded"
+  );
+  ajax_pop.send(`nb_posts=${nb_posts}`);
 
-	ajax_pop.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			var result = JSON.parse(this.responseText);
-			var offset = nb_posts;
-			var main_div_recent = document.querySelector('#recent');
-			main_div_recent.innerHTML = '';
+  ajax_pop.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var result = JSON.parse(this.responseText);
+      var offset = nb_posts;
+      var main_div_recent = document.querySelector("#recent");
+      main_div_recent.innerHTML = "";
 
-			for (var i = 0; i < result.length && i < offset; i++) {
-				var biz_name = result[i].biz_name;
-				var biz_level = result[i].biz_level;
-				var biz_id = result[i].biz_id;
+      for (var i = 0; i < result.length && i < offset; i++) {
+        var biz_name = result[i].biz_name;
+        var biz_level = result[i].biz_level;
+        var biz_id = result[i].biz_id;
 
-				main_div_recent.innerHTML += `<div class="col-xs-12 col-sm-6 col-md-4 business-post">
+        main_div_recent.innerHTML += `<div class="col-xs-12 col-sm-6 col-md-4 business-post">
 					<div class="thumb-box">
 						<div class="thumb-img">
-							<a href="php/mamalee_level_${biz_level}.php?biz_id=${biz_id}">
-								<img src=${result[i]['file_path'].replace('/var/www/html/', 'public/')} width="100%" alt="" />
+							<a href="php/mamalee_level_${biz_level}.php?biz_id=${biz_id}" id="${biz_name}">
+								<img src=${result[i]["file_path"].replace(
+                  "/var/www/html/",
+                  "public/"
+                )} width="100%" alt="" />
 							</a>
 						</div>
 						<div class="thumb-content">
@@ -142,13 +158,13 @@ function loadByPopular(nb_posts) {
 						</div>
 					</div>
 				</div>`;
-			}
+      }
 
-			var recent_button = document.querySelector('#recent_button');
-			recent_button.innerHTML = `<button class="btn btn-more" data-toggle="collapse" data-target="#moreThumb">See More</button>`;
-			loadMore('popular');
-		}
-	};
+      var recent_button = document.querySelector("#recent_button");
+      recent_button.innerHTML = `<button class="btn btn-more" data-toggle="collapse" data-target="#moreThumb">See More</button>`;
+      loadMore("popular");
+    }
+  };
 }
 
 /**
@@ -161,27 +177,30 @@ function loadByPopular(nb_posts) {
  */
 
 function loadTrends(nb_trends) {
-	ajax_trend = new XMLHttpRequest();
-	ajax_trend.open('POST', 'php/trends_db.php', true);
-	ajax_trend.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	ajax_trend.send(`nb_posts=${nb_trends}`);
+  ajax_trend = new XMLHttpRequest();
+  ajax_trend.open("POST", "php/trends_db.php", true);
+  ajax_trend.setRequestHeader(
+    "Content-Type",
+    "application/x-www-form-urlencoded"
+  );
+  ajax_trend.send(`nb_posts=${nb_trends}`);
 
-	ajax_trend.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			var result = JSON.parse(this.responseText);
-			var offset = nb_trends;
-			var main_div_recent = document.querySelector('#recent');
-			main_div_recent.innerHTML = '';
+  ajax_trend.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var result = JSON.parse(this.responseText);
+      var offset = nb_trends;
+      var main_div_recent = document.querySelector("#recent");
+      main_div_recent.innerHTML = "";
 
-			for (var i = 0; i < result.length && i < offset; i++) {
-				var headline = result[i].trend_headline;
-				var article = result[i].trend_article;
-				var id = result[i].trend_id;
-				console.log(result[i].file_path);
-				var filepath = result[i].file_path.replace('../public', 'public');
-				console.log(filepath);
+      for (var i = 0; i < result.length && i < offset; i++) {
+        var headline = result[i].trend_headline;
+        var article = result[i].trend_article;
+        var id = result[i].trend_id;
+        console.log(result[i].file_path);
+        var filepath = result[i].file_path.replace("../public", "public");
+        console.log(filepath);
 
-				main_div_recent.innerHTML += `
+        main_div_recent.innerHTML += `
 				<div class="col-xs-12 col-sm-6 col-md-4 trend-post">
 					<div class="thumb-box">
 						<div class="thumb-img">
@@ -200,13 +219,13 @@ function loadTrends(nb_trends) {
 						</div>
 					</div>
 				</div>`;
-			}
+      }
 
-			var recent_button = document.querySelector('#recent_button');
-			recent_button.innerHTML = `<button class="btn btn-more" data-toggle="collapse" data-target="#moreThumb">See More</button>`;
-			loadMore('trends');
-		}
-	};
+      var recent_button = document.querySelector("#recent_button");
+      recent_button.innerHTML = `<button class="btn btn-more" data-toggle="collapse" data-target="#moreThumb">See More</button>`;
+      loadMore("trends");
+    }
+  };
 }
 
 /**
@@ -217,22 +236,59 @@ function loadTrends(nb_trends) {
  */
 
 function loadMore(currentState) {
-	document.querySelector('.btn-more').addEventListener('click', () => {
-		let nb_business_post = document.querySelectorAll('.business-post').length;
-		let nb_trend_post = document.querySelectorAll('.trend-post').length;
-		switch (currentState) {
-			case 'recent': {
-				loadByRecent(nb_business_post + 3);
-				break;
-			}
-			case 'popular': {
-				loadByPopular(nb_business_post + 3);
-				break;
-			}
-			case 'trends': {
-				loadTrends(nb_trend_post + 3);
-				break;
-			}
-		}
-	});
+  document.querySelector(".btn-more").addEventListener("click", () => {
+    let nb_business_post = document.querySelectorAll(".business-post").length;
+    let nb_trend_post = document.querySelectorAll(".trend-post").length;
+    switch (currentState) {
+      case "recent": {
+        loadByRecent(nb_business_post + 3);
+        break;
+      }
+      case "popular": {
+        loadByPopular(nb_business_post + 3);
+        break;
+      }
+      case "trends": {
+        loadTrends(nb_trend_post + 3);
+        break;
+      }
+    }
+  });
 }
+
+/**
+ * ----------------------------------------------------------------------------------
+ * public
+ * clickCounter: updates the view count by click
+ * ----------------------------------------------------------------------------------
+ */
+
+function clickCounter() {
+  ajax_view = new XMLHttpRequest();
+  ajax_view.open("POST", "./php/click_counter.php", true);
+  ajax_view.setRequestHeader(
+    "Content-Type",
+    "application/x-www-form-urlencoded"
+  );
+  ajax_view.send();
+  ajax_view.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var result = JSON.parse(this.responseText);
+      var biz_click = document.querySelector("#" + biz_id);
+      biz_click.onclick = function() {
+        viewCounter();
+      };
+      for (var i = 0; i < result.length && i < offset; i++) {
+        var biz_view_cnt = result[i].biz_view_cnt;
+        function viewCounter() {
+          biz_view_cnt += 1;
+        }
+      }
+    }
+  };
+}
+clickCounter();
+
+// var biz_click = document.querySelector("#" + biz_id);
+// biz_click.onclick = function() {clickCounter()};
+// function clickCounter(){}
